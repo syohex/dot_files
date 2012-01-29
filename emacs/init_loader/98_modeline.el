9e59a721a8924a9f5aff51e28108bef779eeb9c9
Syohei YOSHIDA <syohex@gmail.com>
2012-01-29 16:29:40 +0900

    remove statement about smarttab

--- emacs/init_loader/98_modeline.el
+++ emacs/init_loader/98_modeline.el
@@ -3,4 +3,3 @@
 ;; short names of minor-mode
 (setcar (cdr (assq 'flymake-mode minor-mode-alist)) " Fm")
 (setcar (cdr (assq 'paredit-mode minor-mode-alist)) " Pe")
-(setcar (cdr (assq 'smart-tab-mode minor-mode-alist)) " St")
