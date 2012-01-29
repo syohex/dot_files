c62e3d99f7f82c1acbf40964d65e53f6e9756019
Syohei YOSHIDA <syohex@gmail.com>
2012-01-29 23:20:05 +0900

    Comment out for inline string rectangle because this function diffrent from string-rectangle a little.

--- emacs/init_loader/04_multimark.el
+++ emacs/init_loader/04_multimark.el
@@ -2,8 +2,8 @@
 ;; (shell-command "cd ~/.emacs.d && git clone https://github.com/magnars/mark-multiple.el.git")
 (add-to-list 'load-path "~/.emacs.d/mark-multiple.el")
 
-(require 'inline-string-rectangle)
-(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+;;(require 'inline-string-rectangle)
+;;(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
 (require 'mark-more-like-this)
 (global-set-key (kbd "C-<") 'mark-previous-like-this)
