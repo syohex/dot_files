62f296fd8f1d80a19d1ddd4779c70b8e873caf35
Syohei YOSHIDA <syohex@gmail.com>
2012-01-31 00:27:45 +0900

    add repos dir and snippet symlink

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -26,7 +26,8 @@ ln -s $PWD/emacs ~/.emacs
 rm -f ~/.emacs.d/init_loader
 ln -s $PWD/init_loader ~/.emacs.d/init_loader
 
-# Create symbolic link of elisp which is managed by git
-rm -f ~/.emacs.d/repos
-ln -s $PWD/repos ~/.emacs.d/repos
+mkdir ~/.emacs.d/repos
 
+# Create symbolic link of elisp which is managed by git
+rm -f ~/.emacs.d/my_snippets
+ln -s $PWD/my_snippets ~/.emacs.d/my_snippets
