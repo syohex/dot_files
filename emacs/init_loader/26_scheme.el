5a3d1cf1adfd9b83af19e1b3a59545d1308c2754
Syohei YOSHIDA <syohex@gmail.com>
2012-02-01 23:13:54 +0900

    add search info function

--- emacs/init_loader/26_scheme.el
+++ emacs/init_loader/26_scheme.el
@@ -11,3 +11,14 @@
 
 (define-key scheme-mode-map (kbd "C-c S") 'scheme-other-window)
 (push '("*scheme*" :stick t) popwin:special-display-config)
+
+(defun gauche-info-index (topic)
+  (interactive
+   (list (read-string
+	  (concat "Gauche help topic : ")
+          (current-word))))
+  (switch-to-buffer-other-window (get-buffer-create "*info*"))
+  (info "/usr/share/info/gauche-refe.info.gz")
+  (Info-index topic))
+
+(define-key global-map "\C-xH" 'gauche-info-index)
