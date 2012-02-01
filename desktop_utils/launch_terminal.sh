de3969f616501f0496f019c9d2ea83416778b14e
Syohei YOSHIDA <syohex@gmail.com>
2012-02-01 23:04:50 +0900

    add utilies of X Window Applications

---
+++ desktop_utils/launch_terminal.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if which gnome-terminal > /dev/null 2>&1
+then
+    exec env VTK_CJK_WIDTH=1 gnome-terminal --hide-menubar --disable-factory
+fi
+
+if which xfce4-terminal > /dev/null 2>&1
+then
+    exec xfce4-terminal --hide-menubar
+fi
+
+zenity --error --text "Please install Terminal Emulator Programs!!"
+
