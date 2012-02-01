4ba62fca5d72ed3daf7c4abca46e68739643ac16
Syohei YOSHIDA <syohex@gmail.com>
2012-02-01 23:14:11 +0900

    add disable mode

--- emacs/init_loader/04_region.el
+++ emacs/init_loader/04_region.el
@@ -3,4 +3,9 @@
 ;; wrap-region
 (require 'wrap-region)
 (wrap-region-global-mode t)
+
+;; disable paredit enable mode
 (add-to-list 'wrap-region-except-modes 'emacs-lisp-mode)
+(add-to-list 'wrap-region-except-modes 'scheme-mode)
+(add-to-list 'wrap-region-except-modes 'lisp-mode)
+(add-to-list 'wrap-region-except-modes 'clojure-mode)
