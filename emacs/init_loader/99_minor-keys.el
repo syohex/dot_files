01cc3bbbaa5a79ce89671024a4c04418e9456687
Syohei YOSHIDA <syohex@gmail.com>
2012-02-16 10:08:41 +0900

    add overriding-mode

--- emacs/init_loader/99_minor-keys.el
+++ emacs/init_loader/99_minor-keys.el
@@ -1,2 +1,9 @@
 ;;;; minor key setting
 (define-key hs-minor-mode-map (kbd "C-#") 'hs-toggle-hiding)
+
+;; (makunbound 'overriding-minor-mode-map)
+(define-minor-mode overriding-minor-mode
+  "Most superior minir mode"
+  t  ;; default is enable
+  "" ;; Not display mode-line
+  `((,(kbd "C-j") . dabbrev-expand)))
