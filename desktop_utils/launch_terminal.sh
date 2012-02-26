6fa1c4cba823b5320a4eaabf3ef0278cb282a5b5
Syohei YOSHIDA <syohex@gmail.com>
2012-02-26 17:18:25 +0900

    Set working directory to home directory instead of root

--- desktop_utils/launch_terminal.sh
+++ desktop_utils/launch_terminal.sh
@@ -2,7 +2,8 @@
 
 if which gnome-terminal > /dev/null 2>&1
 then
-    exec env VTK_CJK_WIDTH=1 gnome-terminal --hide-menubar --disable-factory
+    exec env VTK_CJK_WIDTH=1 gnome-terminal --hide-menubar --disable-factory \
+            --working-directory=$HOME
 fi
 
 if which xfce4-terminal > /dev/null 2>&1
