7e381f3d38a8f6c91fe89c99efcb8ab2a92c5a10
Syohei YOSHIDA <syohex@gmail.com>
2012-03-11 23:56:22 +0900

    fix coffee-mode tab width

--- emacs/init_loader/22_coffeescript.el
+++ emacs/init_loader/22_coffeescript.el
@@ -3,3 +3,6 @@
 (autoload 'coffee-mode "coffee-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(custom-set-variables
+ '(coffee-tab-width 4))
