2575255f0e79afa81090b07d525245444629f7fb
Syohei YOSHIDA <syohex@gmail.com>
2012-03-29 10:57:44 +0900

    fix for emux

--- emacs/init_loader/29_shell.el
+++ emacs/init_loader/29_shell.el
@@ -3,24 +3,24 @@
           'comint-watch-for-password-prompt)
 
 ;; emux
-(autoload 'emux "emux" nil t)
-(eval-after-load "emux"
-  '(progn
-     (global-set-key (kbd "C-!") 'emux:term)
-     ;; avoid abnormal exit
-     (defvar emux:term-last-dir nil)
-     (defadvice emux:term (before emux:term-save-last-directory activate)
-       (setq emux:term-last-dir default-directory))
-     (define-key term-raw-map (kbd "C-c c") 'emux:term-new)
-     (define-key term-raw-map (kbd "C-c n") 'emux:term-next)
-     (define-key term-raw-map (kbd "C-c p") 'emux:term-previous)
+(require 'emux)
+(global-set-key (kbd "C-!") 'emux:term)
+;; avoid abnormal exit
+(defvar emux:term-last-dir nil)
+(defadvice emux:term (before emux:term-save-last-directory activate)
+  (setq emux:term-last-dir default-directory))
 
 ;; ansi-term
-(add-hook 'term-mode-hook
-          '(lambda ()
-             (define-key term-raw-map (kbd "M-x") 'nil)
-             (define-key term-raw-map (kbd "C-z") 'nil)
-             (define-key term-raw-map (kbd "C-q") 'nil)))))
+(defun my/term-mode-hook
+  (define-key term-raw-map (kbd "M-x") 'nil)
+  (define-key term-raw-map (kbd "C-z") 'nil)
+  (define-key term-raw-map (kbd "C-q") 'nil)
+
+  (define-key term-raw-map (kbd "C-c c") 'emux:term-new)
+  (define-key term-raw-map (kbd "C-c n") 'emux:term-next)
+  (define-key term-raw-map (kbd "C-c p") 'emux:term-previous))
+
+(add-hook 'term-mode-hook 'my/term-mode-hook)
 
 ;; color setting
 (setq ansi-term-color-vector
