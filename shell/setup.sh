5119240c46cf9dbd2099f9eef142138181da561c
Syohei YOSHIDA <syohex@gmail.com>
2012-03-29 12:15:48 +0900

    remove uneed setting

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-ln -s $PWD/zshrc ~/.zshrc
+ln -sf $PWD/zshrc ~/.zshrc
 
 # for cdd
-if [ ! -d ~/.zsh ]
-then
-    mkdir -p ~/.zsh
-fi
-
-CDD_PWD_FILE=$HOME/.zsh/cdd_pwd_list
 if [ ! -d ~/.zsh/cdd ]
 then
     mkdir -p ~/.zsh/cdd
-fi
-
-if [ ! -f "$CDD_PWD_FILE" ]; then
-    echo "\n" > "$CDD_PWD_FILE"
-    if [ $? = 1 ]
-    then
-        echo "Error: Don't wrote $CDD_PWD_FILE."
-        return 1
-    fi
 fi
 
 # setting for GNU screen
@@ -28,3 +13,13 @@ ln -sf $PWD/screenrc ~/.screenrc
 
 # setting for tmux
 ln -sf $PWD/tmux.conf ~/.tmux.conf
+
+# for completion file
+COMPDIR=~/.zsh/mycomp
+if [ ! -d $COMPDIR ]
+then
+    mkdir -p $COMPDIR
+fi
+
+curl -o $COMPDIR/_perlbrew  https://raw.github.com/lapis25/dotfiles/master/.zsh/functions/_perlbrew
+curl -o $COMPDIR/_gisty https://raw.github.com/swdyh/gisty/master/_gisty
