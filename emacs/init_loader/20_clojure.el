52a05f9a338641b9a5f529f82d690e6e8b01c708
Syohei YOSHIDA <syohex@gmail.com>
2012-04-02 11:15:49 +0900

    add auto-mode-alist setting

--- emacs/init_loader/20_clojure.el
+++ emacs/init_loader/20_clojure.el
@@ -1,13 +1,3 @@
 ;; setup for clojure
-(autoload 'clojure-mode "clojure-mode")
-
-(defun my/clojure-mode-hook ()
-  (if (featurep 'slime-mode)
-      (call-interactively 'slime-mode)))
-
-(add-hook 'clojure-mode-hook #'my/clojure-mode-hook)
-
-(eval-after-load "clojure-mode"
-  '(progn
-     (require 'inf-clojure)
-     (push '("*clojure*" :stick t) popwin:special-display-config)))
+(add-to-list 'auto-mode-alist "\.clj$")
+(autoload 'clojure-mode "clojure-mode" nil t)
