004ca89132a3747144ed6b4dace79ccd3d93b5b9
Syohei YOSHIDA <syohex@gmail.com>
2012-06-06 12:19:23 +0900

    modify quickrun bingind

--- emacs/init_loader/23_compile.el
+++ emacs/init_loader/23_compile.el
@@ -1,3 +1,4 @@
 ;; quickrun
 ;; (auto-install-from-url "https://raw.github.com/syohex/emacs-quickrun/master/quickrun.el")
 (autoload 'quickrun "quickrun" nil t)
+(global-set-key (kbd "M-g M-q") 'quickrun)
--- emacs/init_loader/99_global-keys.el
+++ emacs/init_loader/99_global-keys.el
@@ -12,7 +12,6 @@
 (unless window-system
   (define-key global-map (kbd "C-z") my/ctrl-z-map))
 
-(global-set-key (kbd "C-z C-q") 'quickrun)
 (global-set-key (kbd "C-z b") 'anything-bookmarks)
 (global-set-key (kbd "C-z ,") 'elscreen-screen-nickname)
 
