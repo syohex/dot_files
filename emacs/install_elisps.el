8f63c5d37adcb5f301bdf1694ea66c958b61ff91
Syohei YOSHIDA <syohex@gmail.com>
2012-06-11 12:40:57 +0900

    remove some packages which can be installed from ELPA

--- emacs/install_elisps.el
+++ emacs/install_elisps.el
@@ -8,244 +8,83 @@
 (auto-install-update-emacswiki-package-name t)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
-;; for installing el-get
-(url-retrieve
- "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
- (lambda (s)
-   (end-of-buffer)
-   (eval-print-last-sexp)))
-
 ;; anything インタフェース周りの全体的な改善
 (auto-install-batch "anything")
 
-;; key-chord 同時押しの実現
-(install-elisp-from-emacswiki "key-chord.el")
+;; anything-c-moccur
+(auto-install-from-url "http://svn.coderepos.org/share/lang/elisp/anything-c-moccur/trunk/anything-c-moccur.el")
+
+;; anything-hatena-bookmark
+(auto-install-from-url "https://raw.github.com/k1LoW/anything-hatena-bookmark/master/anything-hatena-bookmark.el")
+
+;; anything-git-grep
+(auto-install-from-url "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/screen-base.el")
+(auto-install-from-url "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/screen-vc.el")
+(auto-install-from-url "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/screen-vc-anything.el")
+(auto-install-from-url "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/anything-git-grep.el")
 
 ;; color-moccur Emacs内の grep. 再帰的にもできる. 外部プログラム不要
-(install-elisp-from-emacswiki "color-moccur.el")
+(auto-install-from-emacswiki "color-moccur.el")
 
 ;; moccur-edit moccurで抽出した行に対して変更を加えることができる.
-(install-elisp-from-emacswiki "moccur-edit.el")
+(auto-install-from-emacswiki "moccur-edit.el")
 
 ;; redo+. undoの逆を行う.
-(install-elisp-from-emacswiki "redo+.el")
-
-;; bm.el 行を任意の行を強調表示できる. 強調表示した行の移動もできる
-(install-elisp "http://cvs.savannah.gnu.org/viewvc/*checkout*/bm/bm/bm.el")
+(auto-install-from-emacswiki "redo+.el")
 
 ;; recentf-ext. 最近開いたファイルを表示, オープンすることができる
-(install-elisp-from-emacswiki "recentf-ext.el")
-
-;; htmlize.el emacsのハイライト情報を html化する
-(install-elisp "http://fly.srk.fer.hr/~hniksic/emacs/htmlize.el.cgi")
-
-;; gist. Emacsの gist in githubクライアント
-(install-elisp "https://github.com/defunkt/gist.el/raw/master/gist.el")
+(auto-install-from-emacswiki "recentf-ext.el")
 
 ;; viewer. viewモードの機能強化. Modelineの色変更が簡単にできる
-(install-elisp-from-emacswiki "http://www.emacswiki.org/emacs/download/viewer.el")
-
-;; twittering-mode
-;; twittering-mode shuold be installed from github
-; git clone https://github.com/hayamiz/twittering-mode.git
-
-;; popwin. completionバッファなどのポップアップ化(勝手に消してくれる)
-(install-elisp "https://github.com/m2ym/popwin-el/raw/master/popwin.el")
-(install-elisp "https://raw.github.com/m2ym/popwin-el/master/misc/popwin-w3m.el")
+(auto-install-from-emacswiki "http://www.emacswiki.org/emacs/download/viewer.el")
 
 ;; emacs-w3m
 ;; emacs-w3m should be installed from CVS
 ;; cvs -d :pserver:anonymous@cvs.namazu.org:/storage/cvsroot co emacs-w3m
-(install-elisp "http://www.emacswiki.org/emacs/download/w3m-session.el")
-
-;; wanderlust
-;; wanderlust should be install from github
-
-;; dmacro.el
-(install-elisp "http://www.pitecan.com/papers/JSSSTDmacro/dmacro.el")
-
-;; elscreen, elscreen-wl
-;; elscreen url ftp://ftp.morishima.net/pub/morishima.net/naoto/ElScreen/
+(auto-install-from-url "http://www.emacswiki.org/emacs/download/w3m-session.el")
 
 ;; iman emacsの manの補完
-(install-elisp "http://homepage1.nifty.com/bmonkey/emacs/elisp/iman.el")
+(auto-install-from-url "http://homepage1.nifty.com/bmonkey/emacs/elisp/iman.el")
 
 ;; text-translator 翻訳
 (auto-install-batch "text translator")
 
-;; navi2ch
-;; git clone https://github.com/kaoru6/navi2ch.git
-
 ;; pod-mode
-(install-elisp "https://github.com/renormalist/emacs-pod-mode/raw/master/pod-mode.el")
-
-;; gtags-mode
-(install-elisp
- "http://cvs.savannah.gnu.org/viewvc/*checkout*/global/gtags.el?root=global")
-
-;; git-commit-mode
-(install-elisp
- "https://github.com/rafl/git-commit-mode/raw/master/git-commit.el")
-
-;; markdown-mode
-(install-elisp
- "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
-
-;; deferred.el
-(install-elisp
- "http://github.com/kiwanami/emacs-deferred/raw/master/deferred.el")
-(install-elisp
- "https://github.com/kiwanami/emacs-deferred/raw/master/concurrent.el")
+(auto-install-from-url "https://github.com/renormalist/emacs-pod-mode/raw/master/pod-mode.el")
 
 ;; eldoc-extension
-(install-elisp-from-emacswiki "eldoc-extension.el")
+(auto-install-from-emacswiki "eldoc-extension.el")
 
 ;; savekill
-(install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/savekill.el")
-
-;; zen-coding
-(install-elisp "https://github.com/rooney/zencoding/raw/master/zencoding-mode.el")
-
-;; shell-pop.el
-(install-elisp "http://www.emacswiki.org/emacs/download/shell-pop.el")
-
-;; ac-slime
-(install-elisp "https://github.com/purcell/ac-slime/raw/master/ac-slime.el")
+(auto-install-from-url "http://www.emacswiki.org/cgi-bin/wiki/download/savekill.el")
 
 ;; cl-indent-patche.el
-(install-elisp "http://boinkor.net/lisp/cl-indent-patches.el")
-
-;; e2wm
-(install-elisp "https://raw.github.com/kiwanami/emacs-window-manager/master/e2wm.el")
-(install-elisp "https://raw.github.com/kiwanami/emacs-window-layout/master/window-layout.el")
+(auto-install-from-url "http://boinkor.net/lisp/cl-indent-patches.el")
 
 ;; auto-save-buffer
-(install-elisp "http://0xcc.net/misc/auto-save/auto-save-buffers.el")
-
-;; smartchr.el
-(install-elisp "https://raw.github.com/imakado/emacs-smartchr/master/smartchr.el")
-
-;; clojure-mode
-(install-elisp "https://raw.github.com/jochu/clojure-mode/master/clojure-mode.el")
-
-;; paredit-mode
-(install-elisp "http://mumble.net/~campbell/emacs/paredit.el")
-
-;; undo-tree
-(install-elisp "http://www.dr-qubit.org/undo-tree/undo-tree.el")
-
-;; jaunte.el hit-a-hint
-(install-elisp "https://raw.github.com/kawaguchi/jaunte.el/master/jaunte.el")
-
-;; for flymake perl
-(install-elisp "http://svn.coderepos.org/share/lang/elisp/set-perl5lib/set-perl5lib.el")
+(auto-install-from-url "http://0xcc.net/misc/auto-save/auto-save-buffers.el")
 
 ;; perl-completion
-(install-elisp "http://www.emacswiki.org/emacs/download/perl-completion.el")
+(auto-install-from-url "http://www.emacswiki.org/emacs/download/perl-completion.el")
 
 ;; thing-opt
-(install-elisp "http://www.emacswiki.org/emacs/download/thing-opt.el")
-
-;; el-expectations for unit-test
-(auto-install-batch "el-expectations")
-
-;; coffee-mode.el
-(install-elisp "https://raw.github.com/defunkt/coffee-mode/master/coffee-mode.el")
-
-;; popwin:w3m
-(install-elisp "https://raw.github.com/m2ym/popwin-el/v0.3/misc/popwin-w3m.el")
+(auto-install-from-url "http://www.emacswiki.org/emacs/download/thing-opt.el")
 
 ;; xs-mode
-(install-elisp "http://www.emacswiki.org/emacs/download/xs-mode.el")
-
-;; anything-c-moccur
-(install-elisp "http://svn.coderepos.org/share/lang/elisp/anything-c-moccur/trunk/anything-c-moccur.el")
+(auto-install-from-url "http://www.emacswiki.org/emacs/download/xs-mode.el")
 
 ;; ac-python
-(install-elisp "http://chrispoole.com/downloads/ac-python.el")
-
-;; haml-mode and sass-mode
-(install-elisp "https://raw.github.com/nex3/haml-mode/master/haml-mode.el")
-(install-elisp "https://raw.github.com/nex3/sass-mode/master/sass-mode.el")
-
-;; anything-project
-(install-elisp "https://raw.github.com/imakado/anything-project/master/anything-project.el")
+(auto-install-from-url "http://chrispoole.com/downloads/ac-python.el")
 
 ;; auto-complete-clang
 (auto-install-from-url
  "https://raw.github.com/brianjcj/auto-complete-clang/master/auto-complete-clang.el")
 
-;; pos-tip.el
-(auto-install-from-url "http://www.emacswiki.org/emacs/download/pos-tip.el")
-
-;; quickrun
-(auto-install-from-url "https://raw.github.com/syohex/emacs-quickrun/master/quickrun.el")
-
-;; highlight-column
-(auto-install-from-url "http://www.emacswiki.org/emacs/download/vline.el")
-(auto-install-from-url "http://www.emacswiki.org/emacs/download/col-highlight.el")
-
-;; inf-clojure
-(auto-install-from-url "https://raw.github.com/syohex/emacs-inf-clojure/master/inf-clojure.el")
-
-;; eshell
-(install-elisp "http://www.rubyist.net/~rubikitch/private/esh-myparser.el")
-(install-elisp "http://www.rubyist.net/~rubikitch/private/eshell-pop.el")
-
-;; cycle-buffer
-(install-elisp-from-emacswiki "cycle-buffer.el")
-
-;; anything-hatena-bookmark
-(install-elisp "https://raw.github.com/k1LoW/anything-hatena-bookmark/master/anything-hatena-bookmark.el")
-
-;; anything-git-grep
-(install-elisp "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/screen-base.el")
-(install-elisp "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/screen-vc.el")
-(install-elisp "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/screen-vc-anything.el")
-(install-elisp "https://raw.github.com/mechairoi/config/master/.emacs.d/elisp/anything-git-grep.el")
-
 ;; direx(new dired)
-(install-elisp "https://raw.github.com/m2ym/direx-el/master/direx.el")
-
-;; anything-grep
-(install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/anything-grep.el")
-
-;; smartrep
-(install-elisp "http://www.emacswiki.org/emacs/download/smartrep.el")
-
-;; package.el
-(install-elisp "http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el")
-
-;; for Emacswiki
-(install-elisp-from-emacswiki "yaoddmuse.el")
+(auto-install-from-url "https://raw.github.com/m2ym/direx-el/master/direx.el")
 
 ;; terminal multiplexer
-(install-elisp "https://raw.github.com/m2ym/emux-el/master/emux.el")
+(auto-install-from-url "https://raw.github.com/m2ym/emux-el/master/emux.el")
 
 ;; for source code reading
-(install-elisp "https://raw.github.com/takaishi/succor/master/succor.el")
-
-;; indirect region
-(install-elisp "https://raw.github.com/renard/indirect-region/master/indirect-region.el")
-
-;; wrap-region
-(install-elisp "https://raw.github.com/rejeep/wrap-region/master/wrap-region.el")
-
-;; rust-mode
-(install-elisp "https://raw.github.com/marijnh/rust-mode/master/cm-mode.el")
-(install-elisp "https://raw.github.com/marijnh/rust-mode/master/rust-mode.el")
-
-;; lua-mode
-(install-elisp "https://raw.github.com/immerrr/lua-mode/master/lua-mode.el")
-
-;; jump
-(auto-install-from-url "https://raw.github.com/winterTTr/ace-jump-mode/master/ace-jump-mode.el")
-
-;; c-eldoc.el
-(auto-install-from-emacswiki "c-eldoc.el")
-
-;; elixer-mode
-(auto-install-from-url "https://raw.github.com/secondplanet/elixir-mode/master/elixir-mode.el")
-
-;; dart-mode
-(auto-install-from-url "https://raw.github.com/nex3/dart-mode/master/dart-mode.el")
+(auto-install-from-url "https://raw.github.com/takaishi/succor/master/succor.el")
