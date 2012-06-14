7890b21967b41828d3eff98e151e77e626105fb7
Syohei YOSHIDA <syohex@gmail.com>
2012-06-14 11:27:54 +0900

    remove flymake setting for javascript

--- emacs/init_loader/22_javascript.el
+++ emacs/init_loader/22_javascript.el
@@ -1,24 +1,3 @@
 ;; setting for javascript
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
-
 (setq-default js-auto-indent-flag nil)
-
-;; setting for flymake(PLEASE INSTALL jslint)
-(add-to-list 'flymake-allowed-file-name-masks '("\\.js\\'" flymake-js-init))
-(defun flymake-js-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "jslint" (list "--no-es5" local-file))))
-(defun flymake-js-load ()
-  (interactive)
-  (add-to-list 'flymake-err-line-patterns
-               '("^ *[[:digit:]] \\([[:digit:]]+\\),\\([[:digit:]]+\\)\: \\(.+\\)$"
-                 nil 1 2 3))
-  (flymake-mode t))
-
-(add-hook 'js-mode-hook
-          (lambda ()
-            (flymake-js-load)))
