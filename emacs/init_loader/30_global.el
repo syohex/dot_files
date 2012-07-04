1723d431dc35b7a9499b5a184efaaec2be89b6b1
Syohei YOSHIDA <syohex@gmail.com>
2012-07-05 00:29:26 +0900

    change binding for helm

--- emacs/init_loader/30_global.el
+++ emacs/init_loader/30_global.el
@@ -3,10 +3,10 @@
 (require 'gtags)
 (setq gtags-mode-hook
       '(lambda ()
-         (local-set-key (kbd "M-t") 'gtags-find-tag)
-         (local-set-key (kbd "M-r") 'gtags-find-rtag)
-         (local-set-key (kbd "M-s") 'gtags-find-symbol)
-         (local-set-key (kbd "C-t") 'gtags-pop-stack)))
+         (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+         (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+         (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+         (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
 
 ;;; hook for gtags
 (add-hook 'c-mode-common-hook 'gtags-mode)
