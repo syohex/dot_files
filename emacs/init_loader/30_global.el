5bc6a848d1b5d5c2e412192eb032d342d1cbd3d2
Syohei YOSHIDA <syohex@gmail.com>
2012-07-09 21:31:49 +0900

    use only helm-gtags instead of gtags.el

--- emacs/init_loader/30_global.el
+++ emacs/init_loader/30_global.el
@@ -1,15 +1,13 @@
 ;;;; GNU gtags
-;; (auto-install-from-url "http://cvs.savannah.gnu.org/viewvc/*checkout*/global/gtags.el?root=global")
-(require 'gtags)
-(setq gtags-mode-hook
-      '(lambda ()
-         (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
-         (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
-         (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
-         (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+             (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+             (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
 
 ;;; hook for gtags
-(add-hook 'c-mode-common-hook 'gtags-mode)
-(add-hook 'c++-mode-hook 'gtags-mode)
-(add-hook 'java-mode-hook 'gtags-mode)
-(add-hook 'asm-mode 'gtags-mode)
+(add-hook 'c-mode-common-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'java-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode 'helm-gtags-mode)
