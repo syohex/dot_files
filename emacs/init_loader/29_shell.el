aa6d234dab2eb0bf51e467f62acec35083b634ab
Syohei YOSHIDA <syohex@gmail.com>
2012-07-11 23:31:17 +0900

    remove term-mode setting, because I dislike term in emacs. and add eshell utilities

--- emacs/init_loader/29_shell.el
+++ emacs/init_loader/29_shell.el
@@ -2,36 +2,17 @@
 (add-hook 'comint-output-filter-functions
           'comint-watch-for-password-prompt)
 
-;; emux
-(autoload 'emux:term "emux" nil t)
-(autoload 'emux:term-other-window "emux" nil t)
-(global-set-key (kbd "C-!") 'emux:term-other-window)
-(push '(term-mode :height 0.5 :stick t) popwin:special-display-config)
-
-(defun term-toggle-mode ()
+(defun eshell-other-window ()
+  "Run eshll on other window"
   (interactive)
-  (if (term-in-char-mode)
-      (term-line-mode)
-    (term-char-mode)))
+  (let ((eshell-buffer-name "*eshell pop*"))
+    (switch-to-buffer-other-window
+     (get-buffer-create "*eshell pop*"))
+    (eshell)))
 
-;; ansi-term
-(defun my/term-mode-hook ()
-  (define-key term-raw-map (kbd "M-x") 'nil)
-  (define-key term-raw-map (kbd "C-x") 'nil)
-  (define-key term-raw-map (kbd "C-g") 'nil)
+(require 'em-alias)
+(add-to-list 'eshell-command-aliases-list
+             '("cdp" "cd {git rev-parse --show-toplevel}"))
 
-  (define-key term-mode-map (kbd "C-c C-j") 'term-toggle-mode)
-  (define-key term-raw-escape-map (kbd "C-j") 'term-toggle-mode)
-
-  (define-key term-raw-map (kbd "C-c c") 'emux:term-new)
-  (define-key term-raw-map (kbd "C-c ,") 'emux:term-rename)
-  (define-key term-raw-map (kbd "C-c x") 'emux:term-kill)
-
-  (set-face-background 'emux:tab-bar-face "SpringGreen1"))
-
-(add-hook 'term-mode-hook 'my/term-mode-hook)
-
-;; color setting
-(setq ansi-term-color-vector
-      [unspecified "black" "red1" "lime green" "yellow2"
-                   "DeepSkyBlue3" "magenta2" "cyan2" "white"])
+(global-set-key (kbd "M-g M-s") 'eshell-other-window)
+(push '("*eshell pop*" :stick t) popwin:special-display-config)
