cb48f32b1fb2687b773b94122ca12b8701225c28
Syohei YOSHIDA <syohex@gmail.com>
2012-07-18 06:43:07 +0900

    fix for move char

--- emacs/init_loader/99_minor-keys.el
+++ emacs/init_loader/99_minor-keys.el
@@ -17,16 +17,16 @@
 (defalias 'my/alt-q-prefix my/alt-q-map)
 (define-key overriding-minor-mode-map (kbd "M-q") 'my/alt-q-prefix)
 
-(defun add-hyper-char-to-ace-jump-word-mode (c)
+(defun add-hyper-char-to-ace-jump-char-mode (c)
   (define-key my/alt-q-map
     (read-kbd-macro (string c))
     `(lambda ()
        (interactive)
        (setq ace-jump-query-char ,c)
-       (setq ace-jump-current-mode 'ace-jump-word-mode)
+       (setq ace-jump-current-mode 'ace-jump-char-mode)
        (ace-jump-do (concat "\\b"
                             (regexp-quote (make-string 1 ,c)))))))
 
-(loop for c from ?0 to ?9 do (add-hyper-char-to-ace-jump-word-mode c))
-(loop for c from ?A to ?Z do (add-hyper-char-to-ace-jump-word-mode c))
-(loop for c from ?a to ?z do (add-hyper-char-to-ace-jump-word-mode c))
+(loop for c from ?0 to ?9 do (add-hyper-char-to-ace-jump-char-mode c))
+(loop for c from ?A to ?Z do (add-hyper-char-to-ace-jump-char-mode c))
+(loop for c from ?a to ?z do (add-hyper-char-to-ace-jump-char-mode c))
