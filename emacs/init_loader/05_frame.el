65f13a9ac2e817c2e67457704684a8288df9c893
Syohei YOSHIDA <syohex@gmail.com>
2012-07-25 10:07:40 +0900

    add toggle-fullscreen and its binding

--- emacs/init_loader/05_frame.el
+++ emacs/init_loader/05_frame.el
@@ -2,3 +2,12 @@
 (when window-system
   (setq default-frame-alist
         '((width . 115) (height . 42) (top . 28) (left . 0))))
+
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil
+                       'fullscreen
+                       (cond ((frame-parameter nil 'fullscreen) nil)
+                             (t 'fullboth))))
+
+(global-set-key (kbd "C-x C-z") 'toggle-fullscreen)
