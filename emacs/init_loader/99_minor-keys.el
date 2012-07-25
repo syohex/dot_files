63eb67d8daeadedffb2975afdac482bb5e99bf69
Syohei YOSHIDA <syohex@gmail.com>
2012-07-25 23:06:03 +0900

    fixed ace-jump command

--- emacs/init_loader/99_minor-keys.el
+++ emacs/init_loader/99_minor-keys.el
@@ -17,16 +17,17 @@
 (defalias 'my/alt-q-prefix my/alt-q-map)
 (define-key overriding-minor-mode-map (kbd "M-q") 'my/alt-q-prefix)
 
-(defun add-hyper-char-to-ace-jump-char-mode (c)
+(defun add-hyper-char-to-ace-jump-word-mode (c)
   (define-key my/alt-q-map
     (read-kbd-macro (string c))
     `(lambda ()
        (interactive)
+       (quick-jump-push-marker)
        (setq ace-jump-query-char ,c)
-       (setq ace-jump-current-mode 'ace-jump-char-mode)
+       (setq ace-jump-current-mode 'ace-jump-word-mode)
        (ace-jump-do (concat "\\b"
                             (regexp-quote (make-string 1 ,c)))))))
 
-(loop for c from ?0 to ?9 do (add-hyper-char-to-ace-jump-char-mode c))
-(loop for c from ?A to ?Z do (add-hyper-char-to-ace-jump-char-mode c))
-(loop for c from ?a to ?z do (add-hyper-char-to-ace-jump-char-mode c))
+(loop for c from ?0 to ?9 do (add-hyper-char-to-ace-jump-word-mode c))
+(loop for c from ?A to ?Z do (add-hyper-char-to-ace-jump-word-mode c))
+(loop for c from ?a to ?z do (add-hyper-char-to-ace-jump-word-mode c))
