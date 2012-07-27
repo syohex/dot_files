ccacd281db25a357042c25aff5eea76b19fb6312
Syohei YOSHIDA <syohex@gmail.com>
2012-07-28 00:21:23 +0900

    remove iswitch buffer

--- emacs/init_loader/02_buffer.el
+++ emacs/init_loader/02_buffer.el
@@ -1,24 +1,5 @@
 ;;;; setting about `buffer'
 
-;; iswitchb
-(iswitchb-mode)
-(add-hook 'iswitchb-define-mode-map-hook
-          'iswitchb-my-keys)
-
-(defun iswitchb-my-keys ()
-  "Add my keybindings for iswitchb."
-  (define-key iswitchb-mode-map [right] 'iswitchb-next-match)
-  (define-key iswitchb-mode-map [left] 'iswitchb-prev-match)
-  (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
-  (define-key iswitchb-mode-map " " 'iswitchb-next-match)
-  (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match))
-
-;; iswitchbで表示しないバッファ
-(setq iswitchb-buffer-ignore
-      '(
-        "\\*.+"
-        ))
-
 ;; auto-save
 (require 'auto-save-buffers)
 (run-with-idle-timer 5 t 'auto-save-buffers)
