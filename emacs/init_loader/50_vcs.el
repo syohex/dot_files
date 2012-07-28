33c129d5f2714230acfed89688e3f0899daddb83
Syohei YOSHIDA <syohex@gmail.com>
2012-07-28 09:30:41 +0900

    always disable vc-mode

--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -7,13 +7,6 @@
 ;; toggle-vc-mode
 (setq max-specpdl-size 2000)
 (setq max-lisp-eval-depth 1000)
-(require 'cl)
-(setq toggle-vc-list '(Git SVN CVS))
-(defun toggle-vc-mode ()
-  "toggle vc-mode"
-  (interactive)
-  (if (intersection vc-handled-backends toggle-vc-list)
-      (setq vc-handled-backends (set-difference vc-handled-backends toggle-vc-list))
-    (setq vc-handled-backends (union vc-handled-backends toggle-vc-list))))
 
-(toggle-vc-mode) ;; Default vc-mode is turned off
+;; disable vc-mode
+(setq vc-handled-backends ())
