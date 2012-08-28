80b88104fb9bf01217729fc85b68d6fe3b23f21a
Syohei YOSHIDA <syohex@gmail.com>
2012-08-28 12:29:00 +0900

    add isearch-yank-symbol

--- emacs/init_loader/02_search.el
+++ emacs/init_loader/02_search.el
@@ -1,5 +1,11 @@
 ;;;; setting for searching
 
+(defun isearch-yank-symbol ()
+  (interactive)
+  (isearch-yank-internal (lambda () (forward-symbol 1) (point))))
+
+(define-key isearch-mode-map (kbd "C-M-w") 'isearch-yank-symbol)
+
 ;; setting migemo
 (setq migemo-command "cmigemo")
 (setq migemo-options '("-q" "--emacs"))
