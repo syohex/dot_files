e57edc762869379dcb92127c0a287997775ce25b
Syohei YOSHIDA <syohex@gmail.com>
2012-09-04 06:58:52 +0900

    remove ensime setting

--- emacs/init_loader/25_scala.el
+++ emacs/init_loader/25_scala.el
@@ -1,12 +1,4 @@
 ;;;; Scala language setting
-;; Download scala firstly http://www.scala-lang.org/
-(add-to-list 'load-path "~/.emacs.d/scala-mode")
-
-;;;; Ensime -- Enhanced Scala Interaction Mode
-;; https://github.com/aemoncannon/ensime
-;; (shell-command "git clone https://github.com/aemoncannon/ensime.git ~/.emacs.d/ensime")
-(add-to-list 'load-path "~/.emacs.d/ensime/elisp/")
-
 (require 'scala-mode-auto)
 
 (eval-after-load "scala-mode"
@@ -28,6 +20,3 @@
              ("class" "^[ \\t]*\\(class +\\)\\([^(): ]+\\)" 2)
              ("case class" "^[ \\t]*\\(case class +\\)\\([^(): ]+\\)" 2)
              ("object" "\\(object +\\)\\([^(): ]+\\)" 2)))))
-
-(autoload 'scala-mode "ensime")
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
