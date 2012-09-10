27f0ded3938af88c663022f04674d22b0821e503
Syohei YOSHIDA <syohex@gmail.com>
2012-09-10 11:28:13 +0900

    requiring notifications is Linux only, because it uses Dbus

--- emacs/init_loader/00_utils.el
+++ emacs/init_loader/00_utils.el
@@ -1,9 +1,13 @@
 ;; use cl functions and macros in my config files.
 (require 'cl)
 
-;; I use notification anywhere
-(require 'notifications)
-
 ;; decide system is MacOSX
 (defun macosx-p ()
   (eq system-type 'darwin))
+
+(defun linux-p ()
+  (eq system-type 'gnu/linux))
+
+;; I use notification anywhere
+(when (linux-p)
+  (require 'notifications))
