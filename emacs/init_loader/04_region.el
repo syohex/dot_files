b7d4f2eb19f17c554bd2d8fdb9e0783cb5e879fb
Syohei YOSHIDA <syohex@gmail.com>
2012-09-12 23:23:08 +0900

    add zero width wrap-region hook

--- emacs/init_loader/04_region.el
+++ emacs/init_loader/04_region.el
@@ -4,6 +4,15 @@
 (require 'wrap-region)
 (wrap-region-global-mode t)
 
+(defun my/wrap-region-zero-width ()
+  (let ((region-size (- (region-end) (region-beginning)))
+        (wrapper (wrap-region-find (char-to-string last-input-event))))
+    (when (and (zerop region-size) wrapper)
+      (let ((left (wrap-region-wrapper-left wrapper)))
+        (forward-char (length left))))))
+
+(add-hook 'wrap-region-after-wrap-hook 'my/wrap-region-zero-width)
+
 ;; disable paredit enable mode
 (add-to-list 'wrap-region-except-modes 'emacs-lisp-mode)
 (add-to-list 'wrap-region-except-modes 'scheme-mode)
