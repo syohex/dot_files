421694aeb73a542a9747265a68284018718b9066
Syohei YOSHIDA <syohex@gmail.com>
2012-09-27 22:17:35 +0900

    add ac-look

--- emacs/init_loader/08_auto_complete.el
+++ emacs/init_loader/08_auto_complete.el
@@ -16,3 +16,29 @@
 (define-key ac-complete-mode-map (kbd "C-s") 'ac-isearch)
 (define-key ac-completing-map "\t" 'ac-complete)
 (setq ac-quick-help-delay 0.5)
+
+;; look command with auto-complete
+(defun my/ac-look ()
+  "`look' command with auto-completelook"
+  (interactive)
+  (unless (executable-find "look")
+    (error "Please install `look' command"))
+  (let ((word (thing-at-point 'word)))
+    (unless word
+      (error "not found word"))
+    (let ((cmd (format "look %s" word)))
+      (with-temp-buffer
+        (call-process-shell-command cmd nil t)
+        (split-string-and-unquote (buffer-string) "\n")))))
+
+(defun ac-look ()
+  (interactive)
+  (let ((ac-menu-height 50)
+        (ac-candidate-limit t))
+  (auto-complete '(ac-source-look))))
+
+(defvar ac-source-look
+  '((candidates . my/ac-look)
+    (requires . 2)))
+
+(global-set-key (kbd "C-M-l") 'ac-look)
