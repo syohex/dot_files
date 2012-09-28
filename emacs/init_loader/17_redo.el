2ef81a2da132edd4b2d862dfd5f9186012ceeae4
Syohei YOSHIDA <syohex@gmail.com>
2012-09-28 18:50:22 +0900

    remove unneed setting

--- emacs/init_loader/17_redo.el
+++ emacs/init_loader/17_redo.el
@@ -1,8 +1,5 @@
 ;;;; redo+
-;; (auto-install-from-url-from-emacswiki "redo+.el")
 (require 'redo+)
-(global-set-key (kbd "C-`") 'undo)
-(global-set-key (kbd "C-M-/") 'redo)
 (setq undo-no-redo t)
 (setq undo-limit 600000)
 (setq undo-strong-limit 900000)
