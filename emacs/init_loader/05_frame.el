780f08f0aa6d6d7f77862ae0d72a1d3e08f694ce
Syohei YOSHIDA <syohex@gmail.com>
2012-10-04 19:30:25 +0900

    remove unneed setting. Use Alt-F11 instead of this

--- emacs/init_loader/05_frame.el
+++ emacs/init_loader/05_frame.el
@@ -2,12 +2,3 @@
 (when (and window-system (not (macosx-p)))
   (setq default-frame-alist
         '((width . 115) (height . 42) (top . 28) (left . 0))))
-
-(defun toggle-fullscreen ()
-  (interactive)
-  (set-frame-parameter nil
-                       'fullscreen
-                       (cond ((frame-parameter nil 'fullscreen) nil)
-                             (t 'fullboth))))
-
-(global-set-key (kbd "C-x C-z") 'toggle-fullscreen)
