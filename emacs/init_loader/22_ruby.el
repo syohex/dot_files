b3b26c7bd4b3cc728c97ac6aaf90a98a9237b1ee
Syohei YOSHIDA <syohex@gmail.com>
2012-10-04 10:37:06 +0900

    git add indentation advice

--- emacs/init_loader/22_ruby.el
+++ emacs/init_loader/22_ruby.el
@@ -8,6 +8,25 @@
              (ruby-end-mode)
              (flymake-ruby-load)
 
+             ;; indentation
+             (setq ruby-deep-indent-paren nil)
+             (defadvice ruby-indent-line (after unindent-closing-paren activate)
+               (let ((column (current-column))
+                     (indent nil)
+                     (offset 0))
+                 (save-excursion
+                   (back-to-indentation)
+                   (let ((state (syntax-ppss)))
+                     (setq offset (- column (current-column)))
+                     (when (and (eq (char-after) ?\))
+                                (not (zerop (car state))))
+                       (goto-char (cadr state))
+                       (setq indent (current-indentation)))))
+                 (when indent
+                   (indent-line-to indent)
+                   (and (> offset 0) (forward-char offset)))))
+
+             ;; auto insert pair
              (require 'ruby-electric)
              (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
              (setq ruby-electric-expand-delimiters-list nil)
