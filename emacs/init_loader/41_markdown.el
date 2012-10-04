aaba5a278988ca8272b2690d74032a796531fadb
Syohei YOSHIDA <syohex@gmail.com>
2012-10-04 10:36:53 +0900

    remove unneed comment

--- emacs/init_loader/41_markdown.el
+++ emacs/init_loader/41_markdown.el
@@ -1,5 +1,4 @@
 ;; setting markdown-mode
-;; (auto-install-from-url "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.\\(md\\|mdt\\|mdwn\\)$" . markdown-mode))
 (setq markdown-command "Markdown.pl")
