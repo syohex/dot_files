37605c2660474c674c9194f46bdc6342bf9976ba
Syohei YOSHIDA <syohex@gmail.com>
2012-10-09 14:14:43 +0900

    remove unneed setting

--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -4,9 +4,5 @@
 (setq vc-follow-symlinks t)
 (setq auto-revert-check-vc-info t)
 
-;; toggle-vc-mode
-(setq max-specpdl-size 2000)
-(setq max-lisp-eval-depth 1000)
-
 ;; disable vc-mode
-(setq vc-handled-backends ())
+(setq vc-handled-backends '())
