eff1f30c469af90b85ea40f1c0bdc2abc5e50652
Syohei YOSHIDA <syohex@gmail.com>
2012-10-23 22:50:25 +0900

    remove marmalade

--- emacs/init.el
+++ emacs/init.el
@@ -5,8 +5,7 @@
 
 ;; Emacs package system
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 ;; load environment value
