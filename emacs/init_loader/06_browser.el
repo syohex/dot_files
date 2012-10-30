865a9d39f5a8217dcec4a44d355f56682a7907d5
Syohei YOSHIDA <syohex@gmail.com>
2012-10-31 00:18:19 +0900

    set default browser

--- emacs/init_loader/06_browser.el
+++ emacs/init_loader/06_browser.el
@@ -1,3 +1,6 @@
 ;; configuration of emacs-w3m
 (add-to-list 'load-path "~/.emacs.d/elisps/emacs-w3m")
 (require 'w3m-load)
+
+;; set default browser
+(setq browse-url-browser-function 'browse-url-firefox)
