441bf541ea580370d4ee7d7903bb553b3f359e19
Syohei YOSHIDA <syohex@gmail.com>
2012-11-11 23:18:49 +0900

    fix git-commit-mode setting

--- emacs/init_loader/51_git.el
+++ emacs/init_loader/51_git.el
@@ -1,9 +1,9 @@
 ;;;; git-commit-mode
 (autoload 'git-commit-mode "git-commit" nil t)
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
+
 (eval-after-load "git-commit"
   '(progn
      (set-face-foreground 'git-commit-summary-face nil)
      (set-face-underline  'git-commit-summary-face t)
      (set-face-foreground 'git-commit-nonempty-second-line-face nil)
-     (set-face-bold-p     'git-commit-nonempty-second-line-face nil)))
+     (set-face-bold-p     'git-commit-nonempty-second-line-face nil))  )
