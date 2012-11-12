aeca134241b63e566ae708b0f72493b8a16dc2a1
Syohei YOSHIDA <syohex@gmail.com>
2012-11-12 22:36:52 +0900

    add hook flyspell-mode

--- emacs/init_loader/51_git.el
+++ emacs/init_loader/51_git.el
@@ -3,6 +3,7 @@
 
 (eval-after-load "git-commit"
   '(progn
+     (add-hook 'git-commit-mode-hook 'flyspell-mode)
      (set-face-foreground 'git-commit-summary-face nil)
      (set-face-underline  'git-commit-summary-face t)
      (set-face-foreground 'git-commit-nonempty-second-line-face nil)
