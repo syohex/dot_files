49f91da1280c4fd3c83acfdc07cf2ffbcc30cfc7
Syohei YOSHIDA <syohex@gmail.com>
2012-11-16 14:39:48 +0900

    move no-window setting

--- emacs/init_loader/09_show_paren.el
+++ emacs/init_loader/09_show_paren.el
@@ -6,9 +6,3 @@
   (set-face-attribute 'show-paren-match-face nil
                       :background nil :foreground nil
                       :underline "#ffff00" :weight 'extra-bold))
-
-(when (not window-system)
-  (set-face-background 'show-paren-match-face nil)
-  (set-face-foreground 'show-paren-match-face nil)
-  (set-face-underline  'show-paren-match-face "#ffff00")
-  (set-face-bold-p 'show-paren-match-face 'extra-bold))
--- emacs/init_loader/nw-config.el
+++ emacs/init_loader/nw-config.el
@@ -1,2 +1,7 @@
 ;; backspace
 (normal-erase-is-backspace-mode 0)
+
+(set-face-background 'show-paren-match-face nil)
+(set-face-foreground 'show-paren-match-face nil)
+(set-face-underline  'show-paren-match-face "#ffff00")
+(set-face-bold-p 'show-paren-match-face 'extra-bold)
