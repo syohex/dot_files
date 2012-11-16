26b1e9014a6596218602233db0c70ee45c95a261
Syohei YOSHIDA <syohex@gmail.com>
2012-11-16 14:39:18 +0900

    remove migemo setting because I never use it

--- emacs/init_loader/02_search.el
+++ emacs/init_loader/02_search.el
@@ -5,22 +5,3 @@
   (isearch-yank-internal (lambda () (forward-symbol 1) (point))))
 
 (define-key isearch-mode-map (kbd "C-M-w") 'isearch-yank-symbol)
-
-;; setting migemo
-(setq migemo-command "cmigemo")
-(setq migemo-options '("-q" "--emacs"))
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-(setq migemo-user-dictionary nil)
-(setq migemo-regex-dictionary nil)
-(setq migemo-coding-system 'utf-8-unix)
-
-(load "migemo" 'noerror)
-(eval-after-load "migemo"
-  '(progn
-     (migemo-init)
-     (setq migemo-isearch-enable-p nil)
-
-     (defadvice isearch-done (after migemo-search-ad activate)
-       "Disable migemo"
-       (if migemo-isearch-enable-p
-           (setq migemo-isearch-enable-p nil)))))
