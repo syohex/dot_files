67d48d86318b3de9764214cd7abbffc167abe4b4
Syohei YOSHIDA <syohex@gmail.com>
2012-11-16 12:59:34 +0900

    Change default candidate style
    Because overlay is too slow to display candidates

--- emacs/init_loader/98_input_method.el
+++ emacs/init_loader/98_input_method.el
@@ -9,21 +9,12 @@
 
 (when (require 'mozc nil t)
   (setq default-input-method "japanese-mozc")
-  (setq mozc-candidate-style 'overlay)
+  (setq mozc-candidate-style 'echo-area) ;; overlay is too slow
 
   ;; faces
-  (set-face-attribute 'mozc-cand-overlay-even-face 'nil
-                      :background "aquamarine" :foreground "black")
-  (set-face-attribute 'mozc-cand-overlay-odd-face 'nil
-                      :background "aquamarine" :foreground "black")
-
-  ;; key
-  (define-key mozc-mode-map (kbd "C-c C-o") 'my/toggle-mozc-style)
+;;  (set-face-attribute 'mozc-cand-overlay-even-face 'nil
+;;                      :background "aquamarine" :foreground "black")
+;;  (set-face-attribute 'mozc-cand-overlay-odd-face 'nil
+;;                      :background "aquamarine" :foreground "black")
 
   (global-set-key (kbd "C-o") 'toggle-input-method))
-
-(defun my/toggle-mozc-style ()
-  (interactive)
-  (if (eq mozc-candidate-style 'echo-area)
-      (setq mozc-candidate-style 'overlay)
-    (setq mozc-candidate-style 'echo-area)))
