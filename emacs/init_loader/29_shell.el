d6e7ba97687fa30efff6d59bba319e1e10c77d5f
Syohei YOSHIDA <syohex@gmail.com>
2012-11-16 14:40:25 +0900

    remove eshell setting

--- emacs/init_loader/29_shell.el
+++ emacs/init_loader/29_shell.el
@@ -1,17 +1,3 @@
 ;; not display password in shell-mode
 (add-hook 'comint-output-filter-functions
           'comint-watch-for-password-prompt)
-
-(defun eshell-other-window ()
-  "Run eshell on other window"
-  (interactive)
-  (let ((eshell-buffer-name "*eshell pop*"))
-    (switch-to-buffer-other-window
-     (get-buffer-create "*eshell pop*"))
-    (eshell)))
-
-(require 'em-alias)
-(add-to-list 'eshell-command-aliases-list
-             '("cdp" "cd {git rev-parse --show-toplevel}"))
-
-(global-set-key (kbd "M-g M-s") 'eshell-other-window)
