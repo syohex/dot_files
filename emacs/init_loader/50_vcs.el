123d1a2d618e7b071065f7d67fe18cf22b46ad9b
Syohei YOSHIDA <syohex@gmail.com>
2012-11-16 12:36:42 +0900

    merge git.el to vcs.el

--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -1,4 +1,4 @@
-;; for virsion control system
+;;;; Common VCS setting
 (global-auto-revert-mode 1)
 (setq auto-revert-interval 10)
 (setq vc-follow-symlinks t)
@@ -6,3 +6,16 @@
 
 ;; disable vc-mode
 (setq vc-handled-backends '())
+
+;;; git
+
+;; git-commit-mode
+(autoload 'git-commit-mode "git-commit" nil t)
+
+(eval-after-load "git-commit"
+  '(progn
+     (add-hook 'git-commit-mode-hook 'flyspell-mode)
+     (set-face-foreground 'git-commit-summary-face nil)
+     (set-face-underline  'git-commit-summary-face t)
+     (set-face-foreground 'git-commit-nonempty-second-line-face nil)
+     (set-face-bold-p     'git-commit-nonempty-second-line-face nil)))
