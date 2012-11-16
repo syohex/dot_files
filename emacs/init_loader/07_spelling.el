8f792983e668b15ca23178010a6f761e549e7389
Syohei YOSHIDA <syohex@gmail.com>
2012-11-16 14:40:52 +0900

    add flyspell-mode-map setting

--- emacs/init_loader/07_spelling.el
+++ emacs/init_loader/07_spelling.el
@@ -1,5 +1,11 @@
 ;; configuration of spell check
 (require 'ispell)
 (setq-default ispell-program-name "aspell")
-(eval-after-load "ispell"
- '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+"))
+
+;; flyspell
+(autoload 'flyspell-mode "flyspell" "spell checking at runtime")
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mode-map (kbd "M-n") 'flyspell-goto-next-error)
+     (define-key flyspell-mode-map (kbd "M-.") 'flyspell-auto-correct-word)))
