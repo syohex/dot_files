fdefc6e4776fb497abd8bef428a816de49f3c0fb
Syohei YOSHIDA <syohex@gmail.com>
2012-12-05 16:27:54 +0900

    add pair for org-mode

--- emacs/init_loader/04_region.el
+++ emacs/init_loader/04_region.el
@@ -15,14 +15,10 @@
 (require 'wrap-region)
 (wrap-region-global-mode t)
 
-(defun my/wrap-region-zero-width ()
-  (let ((region-size (- (region-end) (region-beginning)))
-        (wrapper (wrap-region-find (char-to-string last-input-event))))
-    (when (and (zerop region-size) wrapper)
-      (let ((left (wrap-region-wrapper-left wrapper)))
-        (forward-char (length left))))))
-
-(add-hook 'wrap-region-after-wrap-hook 'my/wrap-region-zero-width)
+(wrap-region-add-wrapper "*" "*" nil 'org-mode)
+(wrap-region-add-wrapper "_" "_" nil 'org-mode)
+(wrap-region-add-wrapper "/" "/" nil 'org-mode)
+(wrap-region-add-wrapper "+" "+" nil 'org-mode)
 
 ;; disable paredit enable mode
 (add-to-list 'wrap-region-except-modes 'emacs-lisp-mode)
