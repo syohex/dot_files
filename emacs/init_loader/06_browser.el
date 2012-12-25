089b3dc8c77f13955ce129eda9815f63e8ccd849
Syohei YOSHIDA <syohex@gmail.com>
2012-12-25 23:45:39 +0900

    add utilities

--- emacs/init_loader/06_browser.el
+++ emacs/init_loader/06_browser.el
@@ -4,3 +4,19 @@
 
 ;; set default browser
 (setq browse-url-browser-function 'browse-url-firefox)
+
+;; alc
+(defun alc (word)
+  (interactive
+   (list (read-string "Search Word: " )))
+  (browse-url
+   (format "http://eow.alc.co.jp/%s/UTF-8/?ref=sa" word)))
+
+;; google search
+(defun google (words-str)
+  (interactive
+   (list
+    (read-string "Search Words: ")))
+  (let ((query (mapconcat #'identity (split-string words-str) "+")))
+    (browse-url
+     (format "https://www.google.co.jp/search?&q=%s" query))))
