6e997bc329ea94e07e201f6943354c593179b56b
Syohei YOSHIDA <syohex@gmail.com>
2012-12-25 11:27:15 +0900

    add coffeescript setting

--- emacs/init_loader/22_javascript.el
+++ emacs/init_loader/22_javascript.el
@@ -2,3 +2,11 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (setq-default js-auto-indent-flag nil)
 (add-hook 'js-mode-hook 'flymake-jslint-load)
+
+;; coffeescript
+(defun my/coffee-mode-hook ()
+  (setq coffee-tab-width 2)
+  (local-unset-key (kbd "C-m"))
+  (my/wrap-region-as-autopair))
+
+(add-hook 'coffee-mode-hook 'my/coffee-mode-hook)
