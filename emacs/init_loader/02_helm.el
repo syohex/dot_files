03671d1b2b05c0d9456c2d20adc40a5f909acd69
Syohei YOSHIDA <syohex@gmail.com>
2013-01-01 00:08:41 +0900

    use sgit instead of old one

--- emacs/init_loader/02_helm.el
+++ emacs/init_loader/02_helm.el
@@ -21,37 +21,3 @@
                     :foreground "white" :background nil)
 (set-face-attribute 'helm-ff-directory nil
                     :foreground "white" :background nil :underline t)
-
-;; List files in git repos
-(defun helm-c-sources-git-project-for (pwd)
-  (loop for elt in
-        '(("Modified files" . "--modified")
-          ("Untracked files" . "--others --exclude-standard")
-          ("All controlled files in this project" . nil))
-        for title  = (format "%s (%s)" (car elt) pwd)
-        for option = (cdr elt)
-        for cmd    = (format "git ls-files %s" (or option ""))
-        collect
-        `((name . ,title)
-          (init . (lambda ()
-                    (unless (and (not ,option) (helm-candidate-buffer))
-                      (with-current-buffer (helm-candidate-buffer 'global)
-                        (call-process-shell-command ,cmd nil t nil)))))
-          (candidates-in-buffer)
-          (type . file))))
-
-(defun helm-git-project-topdir ()
-  (file-name-as-directory
-   (replace-regexp-in-string
-    "\n" ""
-    (shell-command-to-string "git rev-parse --show-toplevel"))))
-
-(defun helm-git-project ()
-  (interactive)
-  (let ((topdir (helm-git-project-topdir)))
-    (unless (file-directory-p topdir)
-      (error "I'm not in Git Repository!!"))
-    (let* ((default-directory topdir)
-           (sources (helm-c-sources-git-project-for default-directory)))
-      (helm-other-buffer sources "*helm git project*"))))
-(define-key global-map (kbd "C-;") 'helm-git-project)
--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -8,6 +8,45 @@
 (setq vc-handled-backends '())
 
 ;;; git
+;; sgit
+(require 'sgit)
+(global-set-key (kbd "C-x v g") 'sgit:grep)
+(global-set-key (kbd "C-x v l") 'sgit:log)
+(global-set-key (kbd "C-x v d") 'sgit:diff)
+
+;; List files in git repos
+(defun helm-c-sources-git-project-for (pwd)
+  (loop for elt in
+        '(("Modified files" . "--modified")
+          ("Untracked files" . "--others --exclude-standard")
+          ("All controlled files in this project" . nil))
+        for title  = (format "%s (%s)" (car elt) pwd)
+        for option = (cdr elt)
+        for cmd    = (format "git ls-files %s" (or option ""))
+        collect
+        `((name . ,title)
+          (init . (lambda ()
+                    (unless (and (not ,option) (helm-candidate-buffer))
+                      (with-current-buffer (helm-candidate-buffer 'global)
+                        (call-process-shell-command ,cmd nil t nil)))))
+          (candidates-in-buffer)
+          (type . git-file))))
+
+(defun helm-git-project-topdir ()
+  (file-name-as-directory
+   (replace-regexp-in-string
+    "\n" ""
+    (shell-command-to-string "git rev-parse --show-toplevel"))))
+
+(defun helm-git-project ()
+  (interactive)
+  (let ((topdir (helm-git-project-topdir)))
+    (unless (file-directory-p topdir)
+      (error "I'm not in Git Repository!!"))
+    (let* ((default-directory topdir)
+           (sources (helm-c-sources-git-project-for default-directory)))
+      (helm-other-buffer sources "*helm git project*"))))
+(define-key global-map (kbd "C-;") 'helm-git-project)
 
 ;; git-commit-mode
 (autoload 'git-commit-mode "git-commit" nil t)
