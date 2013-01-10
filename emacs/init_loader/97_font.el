ada6ea60a2e17d56417ca7b5fec69bcd007c7904
Syohei YOSHIDA <syohex@gmail.com>
2013-01-11 00:45:57 +0900

    refactoring

--- emacs/init_loader/97_font.el
+++ emacs/init_loader/97_font.el
@@ -1,12 +1,11 @@
 ;; font-setting for Linux(Ubuntu)
 
-(when (and window-system (not (macosx-p)))
+(when (and window-system (linux-p))
   (with-temp-buffer
-    (shell-command "xdpyinfo" t)
+    (call-process "xdpyinfo" nil t)
     (goto-char (point-min))
     (when (re-search-forward "dimensions:\\s-+\\([0-9]+\\)x\\([0-9]+\\)" nil t)
-      (let ((width (string-to-int (match-string-no-properties 1)))
-            (height (string-to-int (match-string-no-properties 2))))
+      (let ((width (string-to-int (match-string-no-properties 1))))
         (cond
          ((>= width 1900) (set-default-font "VL ゴシック-12"))
          (t (set-default-font "VL ゴシック-10")))))))
