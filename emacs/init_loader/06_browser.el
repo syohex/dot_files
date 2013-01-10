9fe46bcc2072615dba4fe427f2a590e6846ab9bc
Syohei YOSHIDA <syohex@gmail.com>
2013-01-10 14:04:44 +0900

    enable lazy-load

--- emacs/init_loader/06_browser.el
+++ emacs/init_loader/06_browser.el
@@ -1,23 +1,7 @@
 ;; configuration of emacs-w3m
 (add-to-list 'load-path "~/.emacs.d/elisps/emacs-w3m")
-(require 'w3m-load)
+(autoload 'w3m "w3m-load" nil t)
 
 ;; set default browser
-(when (macosx-p)
+(unless (macosx-p)
   (setq browse-url-browser-function 'browse-url-firefox))
-
-;; alc
-(defun alc (word)
-  (interactive
-   (list (read-string "Search Word: " )))
-  (browse-url
-   (format "http://eow.alc.co.jp/%s/UTF-8/?ref=sa" word)))
-
-;; google search
-(defun google (words-str)
-  (interactive
-   (list
-    (read-string "Search Words: ")))
-  (let ((query (mapconcat #'identity (split-string words-str) "+")))
-    (browse-url
-     (format "https://www.google.co.jp/search?&q=%s" query))))
