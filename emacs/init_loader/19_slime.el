f0a6e837e3914400ae7d093280a343164c38b01a
Syohei YOSHIDA <syohex@gmail.com>
2013-01-10 11:47:15 +0900

    enable lazy load

--- emacs/init_loader/19_slime.el
+++ emacs/init_loader/19_slime.el
@@ -1,29 +1,27 @@
 ;;; slime
-(require 'slime)
+(autoload 'slime "slime" nil t)
+(eval-after-load "slime"
+  '(progn
+     ;; SLIME REPL
+     (slime-setup '(slime-repl slime-fancy slime-banner))
 
-;; SLIME REPL
-(slime-setup '(slime-repl slime-fancy slime-banner))
+     (define-key slime-repl-mode-map (kbd "TAB") nil)
 
-(define-key slime-repl-mode-map (kbd "TAB") nil)
+     ;; encoding
+     (setq slime-net-coding-system 'utf-8-unix)
 
-;; encoding
-(setq slime-net-coding-system 'utf-8-unix)
+     ;; for clojure
+     (setq slime-protocol-version 'ignore)
 
-;; for clojure
-(setq slime-protocol-version 'ignore)
+     (defun my/slime-mode-hook ()
+       (define-key slime-mode-map (kbd "C-c C-d C-l") 'helm-hyperspec))
+     (add-hook 'slime-mode-hook 'my/slime-mode-hook)
 
-(defun my/slime-mode-hook ()
-  (define-key slime-mode-map (kbd "C-M-i") 'auto-complete)
-  (define-key slime-mode-map (kbd "C-c C-d C-l") 'helm-hyperspec))
-(add-hook 'slime-mode-hook 'my/slime-mode-hook)
+     ;; face
+     (set-face-foreground 'slime-repl-inputed-output-face "pink1")
 
-;; face
-(set-face-foreground 'slime-repl-inputed-output-face "pink1")
-
-;;;; ac-slime
-(require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'slime-repl-mode))
+      ;;;; ac-slime
+     (require 'ac-slime)
+     (add-hook 'slime-mode-hook 'set-up-slime-ac)
+     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+     (define-key slime-repl-mode-map (kbd "C-M-i") 'auto-complete)))
