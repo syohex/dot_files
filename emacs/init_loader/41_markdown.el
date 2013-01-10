f0b90b6b7d1e460557fe811cd0e15f5db6c8aa29
Syohei YOSHIDA <syohex@gmail.com>
2013-01-10 14:05:27 +0900

    enable lazyload and add ac setting

--- emacs/init_loader/41_markdown.el
+++ emacs/init_loader/41_markdown.el
@@ -1,4 +1,12 @@
 ;; setting markdown-mode
-(require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.\\(md\\|mdt\\|mdwn\\)$" . markdown-mode))
-(setq markdown-command "Markdown.pl")
+(autoload 'markdown-mode "markdown-mode" nil t)
+
+(eval-after-load "markdown-mode"
+  '(progn
+     (setq markdown-command "Markdown.pl")
+
+     (add-hook 'markdown-mode-hook 'my/markdown-mode-hook)))
+
+(defun my/markdown-mode-hook ()
+  (add-to-list 'ac-sources 'ac-source-look))
