51c20ddf5f02edbdaaa5a3406e904b8a07858aff
Syohei YOSHIDA <syohex@gmail.com>
2013-01-16 00:40:12 +0900

    move utility to helm-myutils

--- emacs/init_loader/10_dired.el
+++ emacs/init_loader/10_dired.el
@@ -19,23 +19,3 @@
 ;; wdired
 (require 'wdired)
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
-
-;; helm in dired
-(defvar my/helm-c-current-directory-source
-  `((name . "Current Directory Files")
-    (candidates . (lambda ()
-                    (with-helm-current-buffer
-                      (let ((dirs (directory-files (helm-c-current-directory)))
-                            (filter (lambda (d) (string-match "^\.\.?$" d))))
-                        (remove-if filter dirs)))))
-    (action . (("Open File" . (lambda (c)
-                                (find-file c)))))))
-
-(defun my/helm-find-file-current-directory ()
-  (interactive)
-  (let ((curbuf (current-buffer))
-        (orig-major major-mode))
-    (if (helm-other-buffer 'my/helm-c-current-directory-source "*helm-dired*")
-        (and (eq orig-major 'dired-mode) (kill-buffer curbuf)))))
-
-(global-set-key (kbd "C-x C-p") 'my/helm-find-file-current-directory)
