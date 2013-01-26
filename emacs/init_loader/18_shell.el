032a8aa3ecd67b62f44bf24d83e0c0710698a595
Syohei YOSHIDA <syohex@gmail.com>
2013-01-26 16:13:09 +0900

    Add eshell functions

---
+++ emacs/init_loader/18_shell.el
@@ -0,0 +1,31 @@
+;; eshell
+(defvar eshell-pop-buffer "*eshell-pop*")
+(defvar eshell-prev-buffer nil)
+
+(defun eshell-pop ()
+  (interactive)
+  (setq eshell-prev-buffer (current-buffer))
+  (unless (get-buffer eshell-pop-buffer)
+    (save-window-excursion
+      (pop-to-buffer (get-buffer-create eshell-pop-buffer))
+      (eshell-mode)))
+  (popwin:popup-buffer (get-buffer eshell-pop-buffer) :height 20))
+(global-set-key (kbd "M-g M-s") 'eshell-pop)
+
+(defun eshell/cde ()
+  (let* ((file-name (buffer-file-name eshell-prev-buffer))
+         (dir (or (and file-name (file-name-directory file-name))
+                  (and (eq major-mode 'dired-mode) dired-directory)
+                  (with-current-buffer eshell-prev-buffer
+                    default-directory))))
+    (eshell/cd dir)))
+
+(defun eshell/cdp ()
+  (let* ((cmd "git rev-parse --show-toplevel")
+         (dir (with-temp-buffer
+                (unless (call-process-shell-command cmd nil t)
+                  (error "Here is not Git Repository"))
+                (goto-char (point-min))
+                (buffer-substring-no-properties
+                 (point) (line-end-position)))))
+    (eshell/cd dir)))
