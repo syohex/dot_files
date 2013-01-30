f97a1c3f5d4263e5b2f689a3f98e7058df12ff94
Syohei YOSHIDA <syohex@gmail.com>
2013-01-30 10:20:48 +0900

    Enable delay load

--- emacs/init_loader/14_diff.el
+++ emacs/init_loader/14_diff.el
@@ -1,25 +1,25 @@
 ;; setting for diff-mode and ediff
-(require 'diff-mode)
+(eval-after-load "diff-mode"
+  '(progn
+     (set-face-attribute 'diff-added-face nil
+                         :background nil :foreground "green"
+                         :weight 'normal)
+     (set-face-attribute 'diff-removed-face nil
+                         :background nil :foreground "firebrick1"
+                         :weight 'normal)
 
-(set-face-attribute 'diff-added-face nil
-                    :background nil :foreground "green"
-                    :weight 'normal)
-(set-face-attribute 'diff-removed-face nil
-                    :background nil :foreground "firebrick1"
-                    :weight 'normal)
+     (set-face-attribute 'diff-file-header-face nil
+                         :background nil :weight 'extra-bold)
 
-(set-face-attribute 'diff-file-header-face nil
-                    :background nil :weight 'extra-bold)
+     (set-face-attribute 'diff-refine-change nil
+                         :background nil)
 
-(set-face-attribute 'diff-refine-change nil
-                    :background nil)
-
-(set-face-attribute 'diff-header-face nil
-                    :background nil :weight 'extra-bold)
-(set-face-attribute 'diff-hunk-header-face nil
-                    :foreground "turquoise"
-                    :weight 'extra-bold
-                    :underline t)
+     (set-face-attribute 'diff-header-face nil
+                         :background nil :weight 'extra-bold)
+     (set-face-attribute 'diff-hunk-header-face nil
+                         :foreground "turquoise"
+                         :weight 'extra-bold
+                         :underline t)))
 
 ;; key bindings
 (define-key diff-mode-map (kbd "C-M-n") 'diff-file-next)
