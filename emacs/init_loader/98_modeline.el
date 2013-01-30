97b1e6f4dfcba6139cee2fd4ed576c1ac9297cf6
Syohei YOSHIDA <syohex@gmail.com>
2013-01-30 10:34:42 +0900

    Show git branch information to mode-line

--- emacs/init_loader/98_modeline.el
+++ emacs/init_loader/98_modeline.el
@@ -28,3 +28,23 @@
             (setq mode-name mode-str)))))
 
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+;; Show Git branch information to mode-line
+(let ((cell (or (memq 'mode-line-position mode-line-format)
+		(memq 'mode-line-buffer-identification mode-line-format)))
+      (newcdr '(:eval (concat (my/update-git-branch-mode-line)))))
+  (unless (member newcdr mode-line-format)
+    (setcdr cell (cons newcdr (cdr cell)))))
+
+(defface my/git-branch-mode-line
+  '((t (:foreground "Dark green" :weight bold)))
+  "Face for Git branch in mode-line")
+
+(defun my/update-git-branch-mode-line ()
+  (let* ((branch (replace-regexp-in-string
+                  "\r?\n?$" ""
+                  (shell-command-to-string "git symbolic-ref -q HEAD")))
+         (mode-line-str (if (string-match "^refs/heads/" branch)
+                            (format "[%s]" (substring branch 11))
+                          "[Not Repo]")))
+    (propertize mode-line-str 'face 'my/git-branch-mode-line)))
