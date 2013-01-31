4f5edf51ebd4ce077d301f09f770969590168ee6
Syohei YOSHIDA <syohex@gmail.com>
2013-01-31 14:59:33 +0900

    Remove git-commit-mode setting
      Because I use magit

--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -14,19 +14,6 @@
 (global-set-key (kbd "C-x v d") 'sgit:diff)
 (global-set-key (kbd "C-x v s") 'sgit:status)
 
-;; git-commit-mode
-(autoload 'git-commit-mode "git-commit" nil t)
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . git-commit-mode))
-
-(eval-after-load "git-commit"
-  '(progn
-     (add-to-list 'ac-sources 'ac-source-look)
-     (add-hook 'git-commit-mode-hook 'flyspell-mode)
-     (set-face-foreground 'git-commit-summary-face nil)
-     (set-face-underline  'git-commit-summary-face t)
-     (set-face-foreground 'git-commit-nonempty-second-line-face nil)
-     (set-face-bold-p     'git-commit-nonempty-second-line-face nil)))
-
 ;; git-gutter
 (global-set-key (kbd "C-x v g") 'git-gutter)
 (eval-after-load "git-gutter"
