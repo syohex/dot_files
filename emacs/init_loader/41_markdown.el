95946c8086bf5f54209b5b400b89f7cde8131fc2
Syohei YOSHIDA <syohex@gmail.com>
2013-02-04 23:57:07 +0900

    Add markdown imenu setting

--- emacs/init_loader/41_markdown.el
+++ emacs/init_loader/41_markdown.el
@@ -7,5 +7,17 @@
      (setq markdown-command "Markdown.pl")
      (add-hook 'markdown-mode-hook 'my/markdown-mode-hook)))
 
+(defun my/markdown-imenu-create-index ()
+  (save-excursion
+    (goto-char (point-min))
+    (loop while (re-search-forward "^\\(#+\\)\\s-+\\(.+\\)$" nil t)
+          for hash-marks = (match-string-no-properties 1)
+          for header     = (match-string-no-properties 2)
+          collect
+          (cons (format "H%d: %s" (length hash-marks) header)
+                (match-beginning 0)))))
+
 (defun my/markdown-mode-hook ()
+  (set (make-local-variable 'imenu-create-index-function)
+       'my/markdown-imenu-create-index)
   (add-to-list 'ac-sources 'ac-source-look))
