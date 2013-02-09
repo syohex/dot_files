a3a01f3e1924738a358d3e694b3b60ad747c4b29
Syohei YOSHIDA <syohex@gmail.com>
2013-02-09 15:00:58 +0900

    Fix M-k and M-K setting

--- emacs/init_loader/02_edit.el
+++ emacs/init_loader/02_edit.el
@@ -12,16 +12,20 @@
 (global-set-key (kbd "M-C-SPC") 'my/copy-sexp)
 
 ;; delete-speces
+(defun delete-following-spaces ()
+  (interactive)
+  (let ((orig-point (point)))
+    (save-excursion
+      (if current-prefix-arg
+          (skip-chars-backward " \t")
+        (skip-chars-forward " \t"))
+      (delete-region orig-point (point)))))
+(global-set-key (kbd "M-k") 'delete-following-spaces)
+
 (defun my/join-line ()
   (interactive)
   (join-line -1))
-(global-set-key (kbd "M-k") 'my/join-line)
-
-(defun my/delete-indentation ()
-  (interactive)
-  (forward-line)
-  (delete-indentation))
-(global-set-key (kbd "M-K") 'my/delete-indentation)
+(global-set-key (kbd "M-K") 'my/join-line)
 
 ;; for word delete instead of kill-word and backward-kill-word
 (defun delete-word (arg)
