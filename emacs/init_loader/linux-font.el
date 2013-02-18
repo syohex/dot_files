f7918a69c30689fa374ed23d9e351b7661dc98af
Syohei YOSHIDA <syohex@gmail.com>
2013-02-18 22:21:48 +0900

    Improve Linux font setting

--- emacs/init_loader/linux-font.el
+++ emacs/init_loader/linux-font.el
@@ -1,11 +1,13 @@
 ;; font-setting for Linux(Ubuntu)
 
-(when (and window-system (linux-p))
+;; (set-frame-font "VL ゴシック-12")
+(when window-system
   (with-temp-buffer
     (call-process "xdpyinfo" nil t)
     (goto-char (point-min))
     (when (re-search-forward "dimensions:\\s-+\\([0-9]+\\)x\\([0-9]+\\)" nil t)
-      (let ((width (string-to-int (match-string-no-properties 1))))
-        (cond
-         ((>= width 1900) (set-default-font "VL ゴシック-12"))
-         (t (set-default-font "VL ゴシック-10")))))))
+      (let* ((width (string-to-number (match-string-no-properties 1)))
+             (size (if (>= width 1900) 12 10)))
+        (condition-case err
+            (set-frame-font (format "VL ゴシック-%d" size))
+          (error (message "%s" err)))))))
