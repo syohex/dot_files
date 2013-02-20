9584df33ca03d29dc71bac2ca8eae0909260b540
Syohei YOSHIDA <syohex@gmail.com>
2013-02-20 15:49:38 +0900

    Revert loading environment file

--- emacs/init.el
+++ emacs/init.el
@@ -13,6 +13,7 @@
 ;; load environment value
 (let ((shellenv (concat user-emacs-directory "shellenv.el")))
   (when (file-exists-p shellenv)
+    (load-file shellenv)
     (dolist (path (reverse (split-string (getenv "PATH") ":")))
       (add-to-list 'exec-path path))))
 
