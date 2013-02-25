b4ddb4dc8be21c1c400291cbf1a66694f7002cba
Syohei YOSHIDA <syohex@gmail.com>
2013-02-25 18:14:30 +0900

    Fix my setup file

--- emacs/install_elisps.el
+++ emacs/install_elisps.el
@@ -2,14 +2,28 @@
 
 (require 'cl)
 
+(defvar my/elisp-directory)
+(defvar my/init-loader-directory)
+
 (when load-file-name
-  (setq user-emacs-directory (file-name-nondirectory load-file-name)))
+  (setq user-emacs-directory (file-name-directory load-file-name))
+  (setq custom-theme-directory user-emacs-directory))
+
+(setq my/elisp-directory (concat user-emacs-directory "elisps"))
+(unless (file-directory-p my/elisp-directory)
+  (make-directory my/elisp-directory))
+
+(setq my/init-loader-directory (concat user-emacs-directory "init_loader/"))
+(unless load-file-name
+  (unless (file-symlink-p my/init-loader-directory)
+    (make-symbolic-link (concat default-directory "init_loader")
+                        user-emacs-directory)))
 
 ;; first eval this code block
-(add-to-list 'load-path (concat user-emacs-directory "elisps"))
+(add-to-list 'load-path my/elisp-directory)
 
 ;; check commands
-(dolist (cmd '("emacs" "git" "curl" "cvs"))
+(dolist (cmd '("curl" "cvs"))
   (unless (executable-find cmd)
     (error "Please install %s" cmd)))
 
@@ -80,8 +94,74 @@
 
     ;; git
     magit git-gutter
+
+    init-loader
 ))
 
 (dolist (pack my/install-packages)
   (unless (package-installed-p pack)
     (package-install pack)))
+
+(defun my/download-url (url)
+  (assert (stringp url))
+  (if (zerop (call-process "curl" nil nil nil "-O" url))
+      (message "Success Download %s" url)
+    (message "Failed Download %s" url)))
+
+(defun my/system (cmd)
+  (unless (zerop (call-process-shell-command cmd))
+    (error "%s is failed!!" cmd)))
+
+(defvar my/nonelpa-packages-url
+  '(
+    ;; my own utilities
+    "https://raw.github.com/syohex/emacs-helm-ack/master/helm-ack.el"
+    "https://raw.github.com/syohex/emacs-utils/master/pomodoro.el"
+    "https://raw.github.com/syohex/emacs-utils/master/sgit.el"
+    "https://raw.github.com/syohex/emacs-utils/master/helm-myutils.el"
+
+    ;; 3rd pirty
+    "http://homepage1.nifty.com/bmonkey/emacs/elisp/iman.el"
+    "https://github.com/renormalist/emacs-pod-mode/raw/master/pod-mode.el"
+    "http://www.emacswiki.org/emacs/download/xs-mode.el"
+    "http://static.boinkor.net/lisp/cl-indent-patches.el"
+    ))
+
+;; setup nonelpa packages
+(let ((default-directory my/elisp-directory))
+  (dolist (url my/nonelpa-packages-url)
+    (unless (file-exists-p (file-name-nondirectory url))
+      (my/download-url url))))
+
+;; rsense
+(let ((default-directory my/elisp-directory))
+  (when (not (file-directory-p "rsense"))
+    (my/system "curl -O http://cx4a.org/pub/rsense/rsense-0.3.zip")
+    (my/system "unzip rsense-0.3.zip")
+    (my/system "mv rsense-0.3 rsense")
+    (my/system "rm -f rsense-0.3.zip")))
+
+;; w3m
+(let ((default-directory my/elisp-directory))
+  (when (not (file-directory-p "emacs-w3m"))
+    (my/system "cvs -d :pserver:anonymous@cvs.namazu.org:/storage/cvsroot login")
+    (my/system "cvs -d :pserver:anonymous@cvs.namazu.org:/storage/cvsroot co emacs-w3m")
+    (let ((default-directory (concat default-directory "emacs-w3m")))
+      (my/system "autoconf && ./configure && make"))))
+
+;; Hyperspec
+(let ((default-directory my/elisp-directory))
+  (when (not (file-directory-p "HyperSpec"))
+    (my/download-url "ftp://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-7-0.tar.gz")
+    (my/system "tar xf HyperSpec-7-0.tar.gz")
+    (my/system "rm -f HyperSpec-7-0.tar.gz")))
+
+;; setup theme
+(let ((default-directory custom-theme-directory))
+  (my/download-url
+   "https://raw.github.com/syohex/emacs-reverse-theme/master/reverse-theme.el"))
+
+(load-theme 'reverse t t)
+(enable-theme 'reverse)
+
+(init-loader-load my/init-loader-directory)
