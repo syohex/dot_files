901d59b44e8a5d0dacecd8104493ea6605e60893
Syohei YOSHIDA <syohex@gmail.com>
2013-03-12 16:54:53 +0900

    merge peredit setting to paren file

--- emacs/init_loader/09_paren.el
+++ emacs/init_loader/09_paren.el
@@ -6,3 +6,19 @@
   (set-face-attribute 'show-paren-match-face nil
                       :background nil :foreground nil
                       :underline "#ffff00" :weight 'extra-bold))
+
+;;;; Paredit
+(require 'paredit)
+
+(dolist (hook '(emacs-lisp-mode-hook
+                lisp-interaction-mode-hook
+                lisp-mode-hook
+                ielm-mode-hook
+                scheme-mode-hook
+                inferior-scheme-mode-hook
+                clojure-mode-hook
+                slime-repl-mode-hook))
+  (add-hook hook 'enable-paredit-mode))
+
+(define-key paredit-mode-map (kbd "C-(") 'paredit-forward-barf-sexp)
+(define-key paredit-mode-map (kbd "C-)") 'paredit-forward-slurp-sexp)
