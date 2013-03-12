a9f836b856b2881b649cd64204eaff1551ce4ee0
Syohei YOSHIDA <syohex@gmail.com>
2013-03-12 16:56:05 +0900

    Add binding of 'git-gutter:revert-hunk'

--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -17,6 +17,7 @@
 (global-git-gutter-mode t)
 (global-set-key (kbd "C-x C-a") 'git-gutter:toggle)
 (global-set-key (kbd "C-x =") 'git-gutter:popup-diff)
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
 
 (eval-after-load "git-gutter"
   '(progn
