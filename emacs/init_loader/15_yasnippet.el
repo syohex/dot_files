f1c45864acddfbd982c80fff6e5630dda63d6809
Syohei YOSHIDA <syohex@gmail.com>
2013-03-29 18:58:35 +0900

    remove needless autoload setting

--- emacs/init_loader/15_yasnippet.el
+++ emacs/init_loader/15_yasnippet.el
@@ -17,23 +17,21 @@
   (add-hook hook 'yas/minor-mode-on))
 
 ;; helm interface
-(eval-after-load "helm-config"
-  '(progn
-     (defun my-yas/prompt (prompt choices &optional display-fn)
-       (let* ((names (loop for choice in choices
-                           collect (or (and display-fn (funcall display-fn choice))
-                                       coice)))
-              (selected (helm-other-buffer
-                         `(((name . ,(format "%s" prompt))
-                            (candidates . names)
-                            (action . (("Insert snippet" . (lambda (arg) arg))))))
-                         "*helm yas/prompt*")))
-         (if selected
-             (let ((n (position selected names :test 'equal)))
-               (nth n choices))
-           (signal 'quit "user quit!"))))
-     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
-     (global-set-key (kbd "M-=") 'yas/insert-snippet)))
+(defun my-yas/prompt (prompt choices &optional display-fn)
+  (let* ((names (loop for choice in choices
+                      collect (or (and display-fn (funcall display-fn choice))
+                                  coice)))
+         (selected (helm-other-buffer
+                    `(((name . ,(format "%s" prompt))
+                       (candidates . names)
+                       (action . (("Insert snippet" . (lambda (arg) arg))))))
+                    "*helm yas/prompt*")))
+    (if selected
+        (nth (position selected names :test 'equal) choices)
+      (signal 'quit "user quit!"))))
+
+(custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+(global-set-key (kbd "M-=") 'yas/insert-snippet)
 
 ;; snippet-mode for *.yasnippet files
 (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
