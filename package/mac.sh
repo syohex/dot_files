465025a26cce6e14de857fdbb7bfa13a9ab3dcb8
Syohei YOSHIDA <syohex@gmail.com>
2013-04-06 07:49:39 +0900

    Update for spell checking

--- package/mac.sh
+++ package/mac.sh
@@ -5,4 +5,5 @@ set -e
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
-sudo brew install zsh git ispell ag coreutils
+sudo brew install zsh git ag coreutils
+sudo brew install aspell --lang=en
