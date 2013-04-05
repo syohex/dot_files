e59307ff70921c0baae67832fe8b21964b1dabc4
Syohei YOSHIDA <syohex@gmail.com>
2013-04-05 14:01:43 +0900

    Add xsel for send copied content to clip board

--- package/ubuntu.sh
+++ package/ubuntu.sh
@@ -6,7 +6,7 @@ PACKAGES=zsh git-svn emacs vim subversion build-essential \
     emacs-mozc emacs-mozc-bin ibus-mozc mozc-server mozc-utils-gui \
     dia diffutils nkf lv cvs cvsutils w3m gtk-recordmydesktop \
     curl shutter lftp aspell ispell minicom ttf-vlgothic \
-    wmctrl paco tmux sary
+    wmctrl paco tmux sary xsel
 
 echo "Install packages:"
 sudo aptitude install $PACKAGES
