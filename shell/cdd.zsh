d7a97f98f54e582351fa45b7ef0745a7935c8e3c
Syohei YOSHIDA <syohex@gmail.com>
2013-04-19 17:22:59 +0900

    Remove needless code

--- shell/cdd.zsh
+++ shell/cdd.zsh
@@ -1,43 +1,8 @@
 # -*- mode:shell-script -*-
-# * cdd
-#  $ cdd WINDOW番号 でその screen の WINDOW 番号へ移動
-# するための zsh function
-# 他にも cdadd でショートカットの作成、cddel でショートカットの削除とか
+# * Light weight cdd
 #
-# * 使い方
-# 1.
-# .zshrc でなにはともあれ
-#  autoload -U compinit
-#  compinit
-#  source ~/path/cdd  # (cdd はこのファイル) する
-# を書く
-#
-# 2.
-# .zshrc などで chpwd フックで _reg_pwd_screennum を呼び出す
-#  # 例
-#  function chpwd() {
-#    _reg_pwd_screennum
-#  }
-# 3.
-# enjoy!
-#
-# * Q&A
-# - なんで cdd という名前？
-# -- cd からの type が楽だから。
-#
-# * FIXME/TODO: 誰か直して
-# - compctl という一世代前のコマンドで補完してる。かっこいい補完にしたい。
-# - その他リファクタリング
-#
-# * 備考
-#
-# BSD, GNU どっちの sed でもうごくようにした BK:
-# - GNU は -i "suffix" の場合に "suffix" を処理対象ファイルとみなす (スペースをいれてはいけない)
-# - BSD は -i"" の場合に "" を無視して次の引数を suffix として認識してしまう (うえとあわせると常にバックアップをつくることになる)
-# - BSD は i\ コマンドのあとに改行が必須 (GNU もこれを解釈できる)
-#
-# * author
-# - Yuichi Tateno
+# * Original Author
+#   - Yuichi Tateno
 #
 
 CDD_BASE_DIR=$HOME/.zsh/cdd
@@ -56,7 +21,7 @@ fi
 
 echo "\n" >>! "$CDD_PWD_FILE";
 
-function _reg_pwd_screennum() {
+_reg_pwd_screennum() {
     if [ "$STY" != "" ] || [ "$TMUX" != "" ]; then
         if [ ! -f "$CDD_PWD_FILE" ]; then
             echo "\n" >>! "$CDD_PWD_FILE"
@@ -65,7 +30,7 @@ function _reg_pwd_screennum() {
     fi
 }
 
-function _reg_cdd_pwd() {
+_reg_cdd_pwd() {
     if [ ! -f "$CDD_PWD_FILE" ]; then
         echo "\n" >> "$CDD_PWD_FILE"
         if [ $? = 1 ]; then
@@ -78,38 +43,7 @@ function _reg_cdd_pwd() {
 $1:$2" "$CDD_PWD_FILE"
 }
 
-function _cdadd {
-    if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "Usage: cdd add name path"
-        echo "Example: cdd add w ~/myworkspace"
-        return 1
-    fi
-
-    local -A real_path
-    real_path=$(perl -MCwd -e 'print Cwd::realpath("$2")');
-    echo "add $1:$real_path"
-    _reg_cdd_pwd "$1" "$real_path"
-}
-
-function _cddel() {
-    if [ -z "$1" ]; then
-        echo "Usage: cdd del name"
-        return 1
-    fi
-    sed -i".t" -e "/^$1:/d" "$CDD_PWD_FILE"
-}
-
-function cdd() {
-    if [ "$1" = "add" ]; then
-        shift
-        _cdadd $@
-        return 0
-    elif [ "$1" = "del" ]; then
-        shift
-        _cddel $@
-        return 0
-    fi
-
+cdd() {
     local -A arg
     arg=`echo $1|cut -d':' -f1`
     if \grep "^$arg:" "$CDD_PWD_FILE" > /dev/null 2>&1 ;then
@@ -123,10 +57,10 @@ function cdd() {
 }
 
 compctl -K _cdd cdd
-functions _cdd() {
+_cdd() {
     reply=(`\grep --color=none -v "^$WINDOW:" "$CDD_PWD_FILE"`)
 }
 
-function chpwd() {
+chpwd() {
     _reg_pwd_screennum
 }
