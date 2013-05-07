28aae4a786e71b549f85d1e5b603f38c6a089077
Syohei YOSHIDA <syohex@gmail.com>
2013-05-07 15:55:15 +0900

    Use js-mode instead of javascript-generic-mode

--- emacs/init_loader/22_javascript.el
+++ emacs/init_loader/22_javascript.el
@@ -1,4 +1,5 @@
 ;; setting for javascript
+(defalias 'javascript-generic-mode 'js-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
 (setq-default js-auto-indent-flag nil)
 (add-hook 'js-mode-hook 'flymake-jslint-load)
