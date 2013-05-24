7edb329b73965dbca3466caedd25f004d79eb1ba
Syohei YOSHIDA <syohex@gmail.com>
2013-05-24 19:14:33 +0900

    Add merge message file to ignore list

--- emacs/init_loader/16_recent.el
+++ emacs/init_loader/16_recent.el
@@ -2,7 +2,8 @@
 (setq recentf-max-saved-items 2000
       recentf-auto-cleanup 600
       recentf-exclude '("/auto-install/" ".recentf" "/repos/" "/elpa/"
-                        "\\.mime-example" "\\.ido.last" "COMMIT_EDITMSG"))
+                        "\\.mime-example" "\\.ido.last"
+                        "COMMIT_EDITMSG" "MERGE_MSG"))
 
 (when window-system
   (run-at-time t 600 'recentf-save-list))
