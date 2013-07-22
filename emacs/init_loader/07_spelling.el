179f3b04bad08293b79de00b256c10ba226b6228
Syohei YOSHIDA <syohex@gmail.com>
2013-07-22 14:15:08 +0900

    Disable C-M-i in flyspell-mode for auto-complete

--- emacs/init_loader/07_spelling.el
+++ emacs/init_loader/07_spelling.el
@@ -7,6 +7,7 @@
 ;; flyspell
 (eval-after-load "flyspell"
   '(progn
+     (define-key flyspell-mode-map (kbd "C-M-i") nil)
      (define-key flyspell-mode-map (kbd "M-n") 'flyspell-goto-next-error)
      (define-key flyspell-mode-map (kbd "M-.") 'ispell-word)))
 
