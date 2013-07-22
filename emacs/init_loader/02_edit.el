6ff42e3e43a95e09ba1daf762c0ad7b54bbec1e6
Syohei YOSHIDA <syohex@gmail.com>
2013-07-23 06:15:00 +0900

    Revert ace-jump-mode setting

--- emacs/init_loader/02_edit.el
+++ emacs/init_loader/02_edit.el
@@ -90,11 +90,10 @@
 (global-set-key (kbd "M-<backspace>") 'my/backward-kill-word)
 
 ;; moving with ace-jump-mode
-(eval-after-load "ace-jump-mode"
-  '(progn
-     (set-face-foreground 'ace-jump-face-foreground "lime green")
-     (set-face-bold-p 'ace-jump-face-foreground t)
-     (set-face-underline-p 'ace-jump-face-foreground t)))
+(require 'ace-jump-mode)
+(set-face-foreground 'ace-jump-face-foreground "lime green")
+(set-face-bold-p 'ace-jump-face-foreground t)
+(set-face-underline-p 'ace-jump-face-foreground t)
 
 (defun backward-symbol (arg)
   (interactive "p")
