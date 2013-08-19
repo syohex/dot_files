bd03ca4aaaece15ad3b87cd6cecc7ce2c5cc04b1
Syohei YOSHIDA <syohex@gmail.com>
2013-08-19 23:35:29 +0900

    Fix for editing html

--- emacs/init_loader/28_html.el
+++ emacs/init_loader/28_html.el
@@ -3,20 +3,15 @@
   (interactive)
   (insert "<br />"))
 
-(defvar html-mode-map nil "keymap used in html-mode")
-(unless html-mode-map
-  (setq html-mode-map (make-sparse-keymap))
-  (define-key html-mode-map (kbd "C-c b") 'html-mode-insert-br))
+(eval-after-load "sgml-mode"
+  '(progn
+     (define-key html-mode-map (kbd "C-c b") 'html-mode-insert-br)))
 
 ;; zen-coding
 (add-hook 'sgml-mode-hook 'zencoding-mode)
 (add-hook 'html-mode-hook 'zencoding-mode)
 
-;; css-mode
-(autoload 'css-mode "css-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.css\\'" . css-mode))
-
-;; for auto-complete
+;; auto-complete for CSS
 (defvar ac-source-css-property-names
   '((candidates . (loop for property in ac-css-property-alist
                         collect (car property)))))
