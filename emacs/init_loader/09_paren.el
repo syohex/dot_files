683e50631d155107a55dad41172d7b66d83b22c7
Syohei YOSHIDA <syohex@gmail.com>
2013-08-21 22:24:21 +0900

    Add ace-jump-word-mode binding for paredit-mode

--- emacs/init_loader/09_paren.el
+++ emacs/init_loader/09_paren.el
@@ -20,3 +20,7 @@
                 clojure-mode-hook
                 slime-repl-mode-hook))
   (add-hook hook 'enable-paredit-mode))
+
+(eval-after-load "paredit"
+  '(progn
+     (define-key paredit-mode-map (kbd "M-q") 'ace-jump-word-mode)))
