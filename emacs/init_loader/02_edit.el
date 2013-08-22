847341a65ae2c7fe3bba4cb8470d5e4850f6290e
Syohei YOSHIDA <syohex@gmail.com>
2013-08-22 23:14:18 +0900

    Fix for case of minus prefix

--- emacs/init_loader/02_edit.el
+++ emacs/init_loader/02_edit.el
@@ -258,8 +258,10 @@
       (setq char (or (aref translation-table-for-input char) char))))
   (kill-region (point)
                (progn
-                 (forward-char 1)
+                 (when (>= arg 0)
+                   (forward-char 1))
                  (search-forward (char-to-string char) nil nil arg)
-                 (backward-char 1)
+                 (when (>= arg 0)
+                   (backward-char 1))
                  (point))))
 (global-set-key (kbd "M-z") 'my/zap-to-char)
