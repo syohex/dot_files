f8c4dd45c9c79200bccfde0c7bb5a3782c56303b
Syohei YOSHIDA <syohex@gmail.com>
2013-08-22 17:58:07 +0900

    Add unwrap command

--- emacs/init_loader/20_c.el
+++ emacs/init_loader/20_c.el
@@ -4,6 +4,7 @@
   (c-set-style "k&r")
   (c-toggle-electric-state -1)
   (define-key c-mode-map (kbd "C-c o") 'ff-find-other-file)
+  (define-key c-mode-map (kbd "C-c C-s") 'my/unwrap-at-point)
   (hs-minor-mode 1)
   (my/setup-symbol-moving)
   (setq c-basic-offset 4))
