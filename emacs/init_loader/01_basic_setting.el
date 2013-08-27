9882141509811c13fa443ef876d3adebb3a345fc
Syohei YOSHIDA <syohex@gmail.com>
2013-08-27 13:09:09 +0900

    Update ido setting

--- emacs/init_loader/01_basic_setting.el
+++ emacs/init_loader/01_basic_setting.el
@@ -154,7 +154,8 @@
 (setq ido-enable-flex-matching t
       ido-create-new-buffer 'always
       ido-max-prospects 8
-      ido-default-file-method 'selected-window)
+      ido-default-file-method 'selected-window
+      ido-auto-merge-work-directories-length -1)
 (global-set-key (kbd "C-x C-p") 'ido-find-file)
 (define-key minibuffer-local-map (kbd "C-h") 'backward-delete-char)
 
