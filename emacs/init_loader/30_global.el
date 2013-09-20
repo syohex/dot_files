d95d2eacc99e31c6161dec0f56c8cad4822c6a9b
Syohei YOSHIDA <syohex@gmail.com>
2013-09-20 18:29:33 +0900

    Re-factoring for helm-gtags
    And change binding pop-stack

--- emacs/init_loader/30_global.el
+++ emacs/init_loader/30_global.el
@@ -1,10 +1,11 @@
 ;;;; GNU gtags
-(add-hook 'helm-gtags-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
-             (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
-             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
-             (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)))
+(defun my/helm-gtags-mode-hook ()
+  (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+  (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+  (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+  (local-set-key (kbd "M-,") 'helm-gtags-pop-stack))
+
+(add-hook 'helm-gtags-mode-hook 'my/helm-gtags-mode-hook)
 
 ;;; Enable helm-gtags-mode
 (dolist (hook '(c-mode-common-hook
