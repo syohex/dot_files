77e275ca59629087843d87f4d9a4982414132318
Syohei YOSHIDA <syohex@gmail.com>
2013-10-04 15:05:40 +0900

    Add my erlang mode hook

--- emacs/init_loader/27_erlang.el
+++ emacs/init_loader/27_erlang.el
@@ -4,6 +4,10 @@
      (require 'erlang-start)
      (require 'erlang-flymake)))
 
+(defun my/erlang-mode-hook ()
+  (setq erlang-electric-newline-inhibit t))
+(add-hook 'erlang-mode-hook 'my/erlang-mode-hook)
+
 ;; distel
 ;;(add-to-list 'load-path (concat user-emacs-directory "elisps/distel/elisp/")
 ;;(require 'distel)
