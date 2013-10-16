61fd64c6a226c6aca7d039153a3ac52d6fd47670
Syohei YOSHIDA <syohex@gmail.com>
2013-10-16 16:47:35 +0900

    Define my own helm-etags-select function

--- emacs/init_loader/02_helm.el
+++ emacs/init_loader/02_helm.el
@@ -54,3 +54,17 @@
     (let ((default-directory topdir)
           (sources (my/helm-git-project-source topdir)))
       (helm-other-buffer sources "*Helm Git Project*"))))
+
+(defun my/helm-etags-select (arg)
+  (interactive "P")
+  (let ((tag  (helm-etags-get-tag-file))
+        (helm-execute-action-at-once-if-one t))
+    (when (or (equal arg '(4))
+              (and helm-etags-mtime-alist
+                   (helm-etags-file-modified-p tag)))
+      (remhash tag helm-etags-cache))
+    (if (and tag (file-exists-p tag))
+        (helm :sources 'helm-source-etags-select :keymap helm-etags-map
+              :input (thing-at-point 'symbol) :buffer "*helm etags*"
+              :default (concat "\\_<" (thing-at-point 'symbol) "\\_>"))
+      (message "Error: No tag file found, please create one with etags shell command."))))
