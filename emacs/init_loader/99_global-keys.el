c4e8248189e106e3d8c63a6732e0f25475b446fc
Syohei YOSHIDA <syohex@gmail.com>
2013-10-16 12:40:59 +0900

    Add auto-complete snippet binding

--- emacs/init_loader/99_global-keys.el
+++ emacs/init_loader/99_global-keys.el
@@ -44,6 +44,7 @@
 (define-key my/ctrl-q-map (kbd "C-c") 'column-highlight-mode)
 
 (define-key my/ctrl-q-map (kbd "h") 'ac-last-quick-help)
+(define-key my/ctrl-q-map (kbd "C-i") 'ac-complete-yasnippet)
 (define-key my/ctrl-q-map (kbd "C-a") 'text-scale-adjust)
 (define-key my/ctrl-q-map (kbd "C-f") 'flyspell-mode)
 (define-key my/ctrl-q-map (kbd "C-m") 'my/toggle-flymake)
