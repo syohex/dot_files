d311db4e4a3e4247e4df43bfa608b2ec10216211
Syohei YOSHIDA <syohex@gmail.com>
2013-10-17 10:54:33 +0900

    Introduce ac-c-headers

--- emacs/init_loader/20_c.el
+++ emacs/init_loader/20_c.el
@@ -1,12 +1,19 @@
 ;;;; C and C++ setting
-;; C coding style
+
+(eval-after-load "cc-mode"
+  '(progn
+     ;; key bindings
+     (define-key c-mode-map (kbd "C-c o") 'ff-find-other-file)
+     (define-key c-mode-map (kbd "C-c C-s") 'my/unwrap-at-point)
+
+     (require 'ac-c-headers)))
+
 (defun my/c-mode-init ()
   (c-set-style "k&r")
   (c-toggle-electric-state -1)
-  (define-key c-mode-map (kbd "C-c o") 'ff-find-other-file)
-  (define-key c-mode-map (kbd "C-c C-s") 'my/unwrap-at-point)
-  (hs-minor-mode 1)
   (my/setup-symbol-moving)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-header-symbols t)
   (setq c-basic-offset 4))
 
 (add-hook 'c-mode-hook 'my/c-mode-init)
