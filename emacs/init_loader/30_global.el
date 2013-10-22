ebd10e1de7a70d08190117e48d784eec4f054319
Syohei YOSHIDA <syohex@gmail.com>
2013-10-22 12:26:18 +0900

    Set binding in eval-after-load, not hook

--- emacs/init_loader/30_global.el
+++ emacs/init_loader/30_global.el
@@ -1,12 +1,11 @@
 ;;;; GNU gtags
-(defun my/helm-gtags-mode-hook ()
-  (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
-  (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
-  (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
-  (local-set-key (kbd "M-,") 'helm-gtags-pop-stack)
-  (local-set-key (kbd "C-t") 'helm-gtags-pop-stack))
-
-(add-hook 'helm-gtags-mode-hook 'my/helm-gtags-mode-hook)
+(eval-after-load "helm-gtags"
+  '(progn
+     (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+     (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+     (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+     (define-key helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)))
 
 ;;; Enable helm-gtags-mode
 (dolist (hook '(c-mode-common-hook
