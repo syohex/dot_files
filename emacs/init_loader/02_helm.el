2fcb49882aa705cd63b3b63ca8211810de91460e
Syohei YOSHIDA <syohex@gmail.com>
2013-10-31 19:34:53 +0900

    Improve for lazy loading

--- emacs/init_loader/02_helm.el
+++ emacs/init_loader/02_helm.el
@@ -1,33 +1,33 @@
 ;;;; helm
-(require 'helm-config)
-(require 'helm-descbinds)
-
-;; helm-ag
-(custom-set-variables
- '(helm-ag-insert-at-point 'symbol))
-
 ;; configuration helm variable
-(setq helm-idle-delay 0.1
-      helm-input-idle-delay 0
-      helm-candidate-number-limit 500)
-(helm-descbinds-mode)
+(custom-set-variables
+ '(helm-idle-delay 0.1)
+ '(helm-input-idle-delay 0)
+ '(helm-candidate-number-limit 500)
+ '(helm-ag-insert-at-point 'symbol)
+ '(helm-find-files-doc-header ""))
 
-(define-key helm-map (kbd "C-p")   'helm-previous-line)
-(define-key helm-map (kbd "C-n")   'helm-next-line)
-(define-key helm-map (kbd "C-M-n") 'helm-next-source)
-(define-key helm-map (kbd "C-M-p") 'helm-previous-source)
+(eval-after-load "helm"
+  '(progn
+     (helm-descbinds-mode)
+
+     (define-key helm-map (kbd "C-p")   'helm-previous-line)
+     (define-key helm-map (kbd "C-n")   'helm-next-line)
+     (define-key helm-map (kbd "C-M-n") 'helm-next-source)
+     (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
+
+     (set-face-attribute 'helm-source-header nil
+                         :height 1.0 :weight 'semi-bold :family nil)))
 
 ;; helm faces
-(require 'helm-files)
-(define-key helm-find-files-map (kbd "C-M-u") 'helm-find-files-down-one-level)
-(setq helm-find-files-doc-header "")
-(set-face-attribute 'helm-ff-file nil
-                    :foreground "white" :background nil)
-(set-face-attribute 'helm-ff-directory nil
-                    :foreground "cyan" :background nil :underline t)
+(eval-after-load "helm-files"
+  '(progn
+     (define-key helm-find-files-map (kbd "C-M-u") 'helm-find-files-down-one-level)
 
-(set-face-attribute 'helm-source-header nil
-                    :height 1.0 :weight 'semi-bold :family nil)
+     (set-face-attribute 'helm-ff-file nil
+                         :foreground "white" :background nil)
+     (set-face-attribute 'helm-ff-directory nil
+                         :foreground "cyan" :background nil :underline t)))
 
 ;; my helm utilities
 (defun my/helm-git-project-source (pwd)
@@ -35,7 +35,7 @@
         '(("Modified Files" . "--modified")
           ("Untracked Files" . "--others --exclude-standard")
           ("All Files" . ""))
-        for cmd = (format "git ls-files %s" option)
+        for cmd = (concat "git ls-files " option)
         collect
         `((name . ,(format "%s (%s)" description pwd))
           (init . (lambda ()
