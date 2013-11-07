ba327e2a75671da397aee0212fddd83079b4c664
Syohei YOSHIDA <syohex@gmail.com>
2013-11-07 11:55:34 +0900

    Improve my own flycheck utility

--- emacs/init_loader/09_flymake.el
+++ emacs/init_loader/09_flymake.el
@@ -1,35 +1,51 @@
 ;; setting for flymake
 
+;; avoid abnormal exit
 (eval-after-load "flymake"
-  '(progn
-     ;; avoid abnormal exit
-     (defadvice flymake-post-syntax-check (before
-                                           flymake-force-check-was-interrupted
-                                           activate)
-       (setq flymake-check-was-interrupted t))))
+  '(defadvice flymake-post-syntax-check (before
+                                         flymake-force-check-was-interrupted
+                                         activate)
+     (setq flymake-check-was-interrupted t)))
+
+(defsubst my/flycheck-enable-mode-p (mode)
+  (or (memq mode my/flycheck-enable-modes) (memq mode '(go-mode))))
+
+(defsubst my/flycheck-reset-function ()
+  (setq flycheck-display-errors-function 'flycheck-display-error-messages))
+
+(defun my/flycheck-list-errors ()
+  (interactive)
+  (setq flycheck-display-errors-function nil)
+  (if (not (my/flycheck-enable-mode-p major-mode))
+      (error "This mode can't use flycheck-mode")
+    (unless flycheck-mode
+      (flycheck-mode +1))
+    (call-interactively 'flycheck-list-errors)))
 
 (defun my/toggle-flymake ()
   (interactive)
-  (if (or (memq major-mode my/flycheck-enable-modes)
-          (memq major-mode '(go-mode)))
-      (call-interactively 'flycheck-mode)
-    (call-interactively 'flymake-mode)))
+  (if (not (my/flycheck-enable-mode-p major-mode))
+      (call-interactively 'flymake-mode)
+    (call-interactively 'flycheck-mode)
+    (my/flycheck-reset-function)))
 
 (defun my/flymake-popup-error-message ()
   (interactive)
-  (let* ((line-no (flymake-current-line-no))
-         (cur-err-info (flymake-find-err-info flymake-err-info line-no))
-         (line-err-info-list (nth 0 cur-err-info))
-         (count (length line-err-info-list)))
-    (while (> count 0)
-      (when line-err-info-list
-        (let* ((err-info (nth (1- count) line-err-info-list))
-               (file (flymake-ler-file err-info))
-               (full-file (flymake-ler-full-file err-info))
-               (text (flymake-ler-text err-info))
-               (line (flymake-ler-line err-info)))
-          (popup-tip (format "[%s] %s" line text))))
-      (decf count))))
+  (if (and (boundp 'flycheck-mode) flycheck-mode)
+      (my/flycheck-reset-function)
+   (let* ((line-no (flymake-current-line-no))
+          (cur-err-info (flymake-find-err-info flymake-err-info line-no))
+          (line-err-info-list (nth 0 cur-err-info))
+          (count (length line-err-info-list)))
+     (while (> count 0)
+       (when line-err-info-list
+         (let* ((err-info (nth (1- count) line-err-info-list))
+                (file (flymake-ler-file err-info))
+                (full-file (flymake-ler-full-file err-info))
+                (text (flymake-ler-text err-info))
+                (line (flymake-ler-line err-info)))
+           (popup-tip (format "[%s] %s" line text))))
+       (decf count)))))
 
 ;; flycheck
 ;; enable flycheck
@@ -53,7 +69,9 @@
                          :background "red")
      (set-face-attribute 'flycheck-warning nil
                          :weight 'bold :underline "darkorange"
-                         :foreground nil :background nil)))
+                         :foreground nil :background nil)
+     (set-face-attribute 'flycheck-error-list-highlight-at-point nil
+                         :background "grey15")))
 
 (defun my/flymake-goto-next-error (arg)
   (interactive "P")
