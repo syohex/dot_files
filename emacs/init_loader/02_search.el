cad0664cbb1f1929f19c08aa8d3cebccef11703f
Syohei YOSHIDA <syohex@gmail.com>
2013-11-08 14:59:28 +0900

    Update anzu setting

--- emacs/init_loader/02_search.el
+++ emacs/init_loader/02_search.el
@@ -10,7 +10,8 @@
 (custom-set-variables
  '(anzu-mode-lighter "")
  '(anzu-deactivate-region t)
- '(anzu-search-threshold 1000))
+ '(anzu-search-threshold 1000)
+ '(anzu-replace-to-string-separator " => "))
 (set-face-attribute 'anzu-mode-line nil
                     :foreground "yellow")
 (set-face-attribute 'anzu-replace-to nil
