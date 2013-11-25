89bb5f9f89d2bfaee6cfbca6893f44ed22367eb5
Syohei YOSHIDA <syohex@gmail.com>
2013-11-25 23:44:34 +0900

    Add move history bindings

--- emacs/init_loader/30_global.el
+++ emacs/init_loader/30_global.el
@@ -4,7 +4,8 @@
      (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
      (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
      (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
-     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+     (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+     (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
      (define-key helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)))
 
 ;;; Enable helm-gtags-mode
