0d036aad5c3600a6eec23e4476d06a4c24fd249c
Syohei YOSHIDA <syohex@gmail.com>
2013-12-19 12:44:09 +0900

    Use default setting command

--- emacs/init_loader/02_edit.el
+++ emacs/init_loader/02_edit.el
@@ -69,36 +69,4 @@
 
 ;; editutil
 (require 'editutil)
-
-(global-set-key [(control shift up)] 'editutil-move-line-up)
-(global-set-key [(control shift down)] 'editutil-move-line-down)
-
-(global-set-key (kbd "C-M-s") 'editutil-forward-char)
-(global-set-key (kbd "C-M-r") 'editutil-backward-char)
-
-(global-set-key (kbd "M-o") 'editutil-edit-next-line)
-(global-set-key (kbd "M-O") 'editutil-edit-previous-line)
-
-(global-set-key (kbd "M-s") 'editutil-unwrap-at-point)
-(global-set-key (kbd "M-r") 'editutil-replace-wrapped-string)
-(global-set-key (kbd "M-z") 'editutil-zap-to-char)
-
-(global-set-key (kbd "M-n") 'editutil-next-symbol)
-(global-set-key (kbd "M-p") 'editutil-previous-symbol)
-
-(global-set-key (kbd "M-k") 'editutil-delete-following-spaces)
-
-(global-set-key (kbd "C-y") 'editutil-yank)
-
-(global-set-key (kbd "M-d") 'editutil-delete-word)
-(global-set-key (kbd "M-<backspace>") 'editutil-backward-delete-word)
-
-(global-set-key (kbd "C-x r N") 'editutil-number-rectangle)
-
-(global-set-key (kbd "C-M-SPC") 'editutil-copy-sexp)
-
-(smartrep-define-key
-    global-map "C-x" '(("j" . 'editutil-insert-newline-without-moving)))
-
-(smartrep-define-key
-    global-map "M-g" '(("c" . 'editutil-duplicate-thing)))
+(editutil-default-setup)
