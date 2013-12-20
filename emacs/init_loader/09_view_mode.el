bd4cd49411f39ea026397925065dda120c62f978
Syohei YOSHIDA <syohex@gmail.com>
2013-12-20 12:19:59 +0900

    fix for view-mode

--- emacs/init_loader/09_view_mode.el
+++ emacs/init_loader/09_view_mode.el
@@ -2,29 +2,33 @@
 (custom-set-variables
  '(view-read-only t))
 
-(defun View-goto-line-last (&optional line)
-  "goto last line"
-  (interactive "P")
-  (goto-line (line-number-at-pos (point-max))))
+(defun View-goto-line-last ()
+  (interactive)
+  (goto-char (point-max))
+  (goto-char (line-beginning-position)))
 
 (eval-after-load "view"
   '(progn
-     ;; less like
      (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
      (define-key view-mode-map (kbd "?") 'View-search-regexp-backward?)
      (define-key view-mode-map (kbd "g") 'View-goto-line)
      (define-key view-mode-map (kbd "G") 'View-goto-line-last)
-     (define-key view-mode-map (kbd "b") 'View-scroll-page-backward)
-     (define-key view-mode-map (kbd "f") 'View-scroll-page-forward)
-     ;; vi/w3m like
+     (define-key view-mode-map (kbd "f") 'editutil-forward-char)
+     (define-key view-mode-map (kbd "F") 'editutil-backward-char)
+     (define-key view-mode-map (kbd "w") 'forward-word)
+     (define-key view-mode-map (kbd "b") 'backward-word)
+
+     (define-key view-mode-map (kbd "i") 'editutil-view-insert)
+     (define-key view-mode-map (kbd "a") 'editutil-view-insert-at-next)
+     (define-key view-mode-map (kbd "I") 'editutil-view-insert-at-bol)
+     (define-key view-mode-map (kbd "A") 'editutil-view-insert-at-eol)
+
      (define-key view-mode-map (kbd "h") 'backward-char)
      (define-key view-mode-map (kbd "j") 'next-line)
      (define-key view-mode-map (kbd "k") 'previous-line)
      (define-key view-mode-map (kbd "l") 'forward-char)
      (define-key view-mode-map (kbd "[") 'backward-paragraph)
-     (define-key view-mode-map (kbd "]") 'forward-paragraph)
-     (define-key view-mode-map (kbd "J") 'View-scroll-line-forward)
-     (define-key view-mode-map (kbd "K") 'View-scroll-line-backward)))
+     (define-key view-mode-map (kbd "]") 'forward-paragraph)))
 
 (eval-after-load "doc-view"
   '(progn
