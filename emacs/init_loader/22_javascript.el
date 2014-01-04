4bab5ef00339553295009b591e94204ee94ce5c2
Syohei YOSHIDA <syohex@gmail.com>
2014-01-04 23:39:28 +0900

    Add binding for no window mode

--- emacs/init_loader/22_javascript.el
+++ emacs/init_loader/22_javascript.el
@@ -15,6 +15,8 @@
      (define-key coffee-mode-map [remap newline-and-indent] 'nil)
      (define-key coffee-mode-map (kbd "C-m") 'nil)
      (define-key coffee-mode-map (kbd "C-<return>") 'coffee-newline-and-indent)
+     (define-key coffee-mode-map (kbd "C-c <") 'coffee-indent-shift-left)
+     (define-key coffee-mode-map (kbd "C-c >") 'coffee-indent-shift-right)
      (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)))
 
 (defun my/coffee-mode-hook ()
