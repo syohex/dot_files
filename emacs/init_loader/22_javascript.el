2f2ddfaead6d978b35f884ed4cdf2e8ba24c2786
Syohei YOSHIDA <syohex@gmail.com>
2014-01-05 12:39:27 +0900

    Add binding for editing next line

--- emacs/init_loader/22_javascript.el
+++ emacs/init_loader/22_javascript.el
@@ -10,11 +10,17 @@
 (custom-set-variables
  '(coffee-tab-width 2))
 
+(defun my/coffee-edit-next-line ()
+  (interactive)
+  (goto-char (line-end-position))
+  (coffee-newline-and-indent))
+
 (eval-after-load "coffee-mode"
   '(progn
      (define-key coffee-mode-map [remap newline-and-indent] 'nil)
      (define-key coffee-mode-map (kbd "C-m") 'nil)
      (define-key coffee-mode-map (kbd "C-<return>") 'coffee-newline-and-indent)
+     (define-key coffee-mode-map (kbd "M-o") 'my/coffee-edit-next-line)
      (define-key coffee-mode-map (kbd "C-c <") 'coffee-indent-shift-left)
      (define-key coffee-mode-map (kbd "C-c >") 'coffee-indent-shift-right)
      (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)))
