ae52b69911a2fe281a29bafdfdc592aec5265b65
Syohei YOSHIDA <syohex@gmail.com>
2014-01-10 23:10:36 +0900

    Don't use C-M-k

--- emacs/init_loader/02_edit.el
+++ emacs/init_loader/02_edit.el
@@ -55,7 +55,7 @@
      (dolist (mode '(ruby-mode python-mode sh-mode js-mode coffee-mode cperl-mode))
        (sp-local-pair mode "'" "'"))
 
-     (let ((disabled '("M-<backspace>" "C-M-n" "C-M-p" "C-M-a" "C-M-e" "C-M-w")))
+     (let ((disabled '("M-<backspace>" "C-M-n" "C-M-p" "C-M-a" "C-M-e" "C-M-w" "C-M-k")))
        (setq sp-smartparens-bindings
              (remove-if (lambda (key-command)
                           (member (car key-command) disabled))
