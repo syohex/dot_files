434a95aedc8e0aeafbf46ec055cca56df3db97c8
Syohei YOSHIDA <syohex@gmail.com>
2014-01-14 17:16:00 +0900

    move slime setting from common_lisp setup file

--- emacs/init_loader/19_slime.el
+++ emacs/init_loader/19_slime.el
@@ -1,5 +1,5 @@
 ;;; slime
-(require 'slime-autoloads)
+;;(require 'slime-autoloads)
 
 (custom-set-variables
  '(slime-net-coding-system 'utf-8-unix))
@@ -21,17 +21,42 @@
 
      (defalias 'slime-cleanup 'slime-restart-inferior-lisp)
 
+     ;; hyperspec
+     (add-to-list 'load-path (concat (file-name-directory (locate-library "slime")) "lib"))
+     (require 'hyperspec)
+     (let ((hyperspec-dir (expand-file-name
+                           (concat user-emacs-directory "elisps/HyperSpec/"))))
+       (setq common-lisp-hyperspec-root (concat "file://" hyperspec-dir)
+             common-lisp-hyperspec-symbol-table (concat hyperspec-dir "Data/Map_Sym.txt")))
+
       ;;;; ac-slime
-     (require 'ac-slime)
      (add-hook 'slime-mode-hook 'set-up-slime-ac)
-     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)))
+     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
-(defun my/slime-mode-hook ()
-  (define-key slime-mode-map (kbd "C-M-i") 'auto-complete)
-  (define-key slime-mode-map (kbd "C-c C-d C-a") 'helm-hyperspec)
-  (define-key slime-mode-map (kbd "C-c C-d C-d") 'common-lisp-hyperspec))
+     ;; bindings
+     (define-key slime-mode-map (kbd "C-M-i") 'auto-complete)
+     (define-key slime-mode-map (kbd "C-c C-d C-a") 'helm-hyperspec)
+     (define-key slime-mode-map (kbd "C-c C-d C-d") 'common-lisp-hyperspec)))
 
-(add-hook 'slime-mode-hook 'my/slime-mode-hook)
+(defun my/obarray-to-list (obarray)
+  (let (symbols)
+    (mapatoms (lambda (symb) (push symb symbols))
+              obarray)
+    symbols))
+
+;; hyperspec with helm
+(defvar helm-hyperspec-source
+  `((name . "Lookup Hyperspec")
+    (candidates . ,(lambda ()
+                     (mapcar 'symbol-name
+                             (my/obarray-to-list common-lisp-hyperspec-symbols))))
+    (action . (("Show Hyperspec" . hyperspec-lookup)))))
+
+(defun helm-hyperspec ()
+  (interactive)
+  (helm :sources '(helm-hyperspec-source)
+        :default (thing-at-point 'symbol)
+        :buffer (get-buffer-create "*Helm HyperSpec*")))
 
 (eval-after-load "slime-repl"
   '(progn
