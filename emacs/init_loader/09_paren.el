d9c6a171823f5110da0ca04101c270a53ef5f065
Syohei YOSHIDA <syohex@gmail.com>
2014-04-16 12:05:54 +0900

    Add toggle-let command binding

--- emacs/init_loader/09_paren.el
+++ emacs/init_loader/09_paren.el
@@ -22,4 +22,5 @@
 
 (eval-after-load "paredit"
   '(progn
+     (define-key paredit-mode-map (kbd "C-c C-l") 'editutil-toggle-let)
      (define-key paredit-mode-map (kbd "M-)") 'move-past-close-and-reindent)))
