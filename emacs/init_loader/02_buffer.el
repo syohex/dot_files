b356680d6b739033c4118cc9a8ce2b06c4e33bb5
Syohei YOSHIDA <syohex@gmail.com>
2014-05-16 12:22:03 +0900

    Remove needless function

--- emacs/init_loader/02_buffer.el
+++ emacs/init_loader/02_buffer.el
@@ -21,22 +21,6 @@
 ;; use ibuffer instead of list-buffer
 (defalias 'list-buffers 'ibuffer)
 
-;; mark 'D'(delete) for matching buffer
-(defun ibuffer-menu-grep-delete (str)
-  (interactive
-   (list (read-string "Delete Mark Regexp: ")))
-  (save-excursion
-    (goto-char (point-min))
-    (forward-line 2)
-    (while (re-search-forward str nil t)
-      (save-excursion
-        (ibuffer-mark-for-delete nil))
-      (end-of-line))))
-
-(eval-after-load "ibuffer"
-  '(progn
-     (define-key ibuffer-mode-map "R" 'ibuffer-menu-grep-delete)))
-
 (global-set-key (kbd "M-9") 'bs-cycle-next)
 (global-set-key (kbd "M-0") 'bs-cycle-previous)
 
