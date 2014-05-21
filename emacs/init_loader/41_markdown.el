90d825515b8ca43eb2afd9f45adb2e61af4b7b58
Syohei YOSHIDA <syohex@gmail.com>
2014-05-21 14:58:17 +0900

    Remove needless hook

--- emacs/init_loader/41_markdown.el
+++ emacs/init_loader/41_markdown.el
@@ -14,18 +14,3 @@
      (define-key markdown-mode-map (kbd "C-c C-f") 'outline-forward-same-level)
      (define-key markdown-mode-map (kbd "C-c C-b") 'outline-backward-same-level)
      (define-key markdown-mode-map (kbd "C-c C-u") 'outline-up-heading)))
-
-(defvar markdown-imenu-generic-expression
-  '(("title"  "^\\(.+?\\)[\n]=+$" 1)
-    ("h2-"    "^\\(.+?\\)[\n]-+$" 1)
-    ("h1"   "^#\\s-+\\(.+?\\)$" 1)
-    ("h2"   "^##\\s-+\\(.+?\\)$" 1)
-    ("h3"   "^###\\s-+\\(.+?\\)$" 1)
-    ("h4"   "^####\\s-+\\(.+?\\)$" 1)
-    ("h5"   "^#####\\s-+\\(.+?\\)$" 1)
-    ("h6"   "^######\\s-+\\(.+?\\)$" 1)
-    ("fn"   "^\\[\\^\\(.+?\\)\\]" 1) ))
-
-(defun my/markdown-mode-hook ()
-  (setq imenu-generic-expression markdown-imenu-generic-expression))
-(add-hook 'markdown-mode-hook 'my/markdown-mode-hook)
