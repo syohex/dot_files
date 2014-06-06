2602a958ac015bec016ea7b407b24eaf487f7240
Syohei YOSHIDA <syohex@gmail.com>
2014-06-06 11:17:28 +0900

    update symlink script

--- create_symlink.sh
+++ create_symlink.sh
@@ -62,6 +62,7 @@ mkdir_if_not_exist ${PERCOL_DIR}
 ln -sf $PWD/python/percolrc.py ${PERCOL_DIR}/rc.py
 
 # Ruby
+ln -sf $PWD/ruby/gemrc ~/.gemrc
 ln -sf $PWD/ruby/rubocop.yml ~/.rubocop.yml
 ln -sf $PWD/ruby/pryrc ~/.pryrc
 
