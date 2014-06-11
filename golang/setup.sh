e23aac0a79a968e723edf6d0e8c804e847a1bcd0
Syohei YOSHIDA <syohex@gmail.com>
2014-06-11 10:55:37 +0900

    add gotentry and peco

--- golang/setup.sh
+++ golang/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 set -e
+set -x
 
 go get -u github.com/nsf/gocode
 go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u code.google.com/p/go.tools/cmd/goimports
 go get -u github.com/golang/lint/golint
+go get -u github.com/lestrrat/peco/cmd/peco/
+go get -u github.com/syohex/gotentry
