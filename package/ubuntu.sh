2587dc837294c2d4a4b7ecad849ab71857437ec6
Syohei YOSHIDA <syohex@gmail.com>
2014-06-22 15:39:09 +0900

    Update package list

--- package/ubuntu.sh
+++ package/ubuntu.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-PACKAGES=zsh git-svn emacs vim subversion build-essential \
-    emacs-mozc emacs-mozc-bin ibus-mozc mozc-server mozc-utils-gui \
-    dia diffutils nkf lv cvs cvsutils w3m gtk-recordmydesktop \
-    curl shutter lftp aspell ispell minicom ttf-vlgothic \
-    wmctrl paco tmux sary xsel
+PACKAGES="zsh git-svn emacs vim subversion build-essential
+    emacs-mozc emacs-mozc-bin mozc-server mozc-utils-gui
+    dia diffutils nkf lv w3m gtk-recordmydesktop
+    curl shutter lftp aspell ispell minicom ttf-vlgothic
+    wmctrl paco tmux sary xsel exuberant-ctags"
+
+if [ "x_$XDG_CURRENT_DESKTOP" = "x_XFCE" ]; then
+    PACKAGES="$PACKAGES xfce-theme-manager"
+fi
 
 echo "Install packages:"
 sudo aptitude install $PACKAGES
