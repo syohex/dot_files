b72c57f02f07eee3fbe93690c018ef5f71958ce0
Syohei YOSHIDA <syohex@gmail.com>
2014-06-24 23:36:57 +0900

    add ghq

--- golang/setup.sh
+++ golang/setup.sh
@@ -9,3 +9,5 @@ go get -u code.google.com/p/go.tools/cmd/goimports
 go get -u github.com/golang/lint/golint
 go get -u github.com/peco/peco/cmd/peco/
 go get -u github.com/syohex/gotentry
+go get -u github.com/motemen/ghq
+
