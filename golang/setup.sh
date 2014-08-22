ccc7bce79416be518348f04f59e623f428d64631
Syohei YOSHIDA <syohex@gmail.com>
2014-08-22 11:57:44 +0900

    add gox

--- golang/setup.sh
+++ golang/setup.sh
@@ -7,11 +7,12 @@ go get -u github.com/nsf/gocode
 go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u code.google.com/p/go.tools/cmd/goimports
 go get -u github.com/golang/lint/golint
-go get -u github.com/peco/peco/cmd/peco/
+go get -u github.com/peco/peco/cmd/peco
 go get -u github.com/syohex/gotentry
 go get -u github.com/motemen/ghq
 go get -u github.com/peco/migemogrep
 go get -u github.com/jstemmer/gotags
+go get -u github.com/mitchellh/gox
 
 OS=$(uname)
 if [ "x$OS" = "xLinux" ]; then
