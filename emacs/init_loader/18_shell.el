ed30e3332ee8c262487452f203435a46ad7c689d
Syohei YOSHIDA <syohex@gmail.com>
2014-09-16 13:50:24 +0900

    Separate my own eshell utilities as library

--- emacs/init_loader/18_shell.el
+++ emacs/init_loader/18_shell.el
@@ -10,6 +10,10 @@
   (set-face-attribute 'compilation-line-number nil :underline t))
 
 ;; eshell
+(with-eval-after-load 'shell-pop
+  (require 'eshellutil)
+  (add-hook 'shell-pop-up-hook 'eshellutil-shell-pop-up-hook))
+
 (with-eval-after-load 'em-prompt
   (set-face-attribute 'eshell-prompt nil
                       :foreground "yellow"))
@@ -25,30 +29,3 @@
  '(shell-pop-universal-key "M-_")
  '(shell-pop-full-span t))
 (global-set-key (kbd "M-_") 'shell-pop)
-
-(defvar my/eshell-prev-buffer nil)
-(defun my/shell-pop-up-hook ()
-  (setq my/eshell-prev-buffer (current-buffer)))
-(add-hook 'shell-pop-up-hook 'my/shell-pop-up-hook)
-
-(defun eshell/cde ()
-  (let* ((file-name (buffer-file-name my/eshell-prev-buffer))
-         (dir (or (and file-name (file-name-directory file-name))
-                  (and (eq major-mode 'dired-mode) dired-directory)
-                  (with-current-buffer my/eshell-prev-buffer
-                    default-directory))))
-    (eshell/cd dir)))
-
-(defun eshell/cdp ()
-  (let ((dir (cl-loop with cwd = default-directory
-                      for d in '(".git" ".hg" ".svn")
-                      when (locate-dominating-file cwd d)
-                      return (file-name-directory it))))
-    (eshell/cd dir)))
-
-(defun eshell/e (file)
-  (let ((curwin (get-buffer-window))
-        (filepath (concat default-directory file)))
-    (other-window 1)
-    (find-file filepath)
-    (delete-window curwin)))
