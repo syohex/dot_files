79fd5d61787110e9a9a3a0bf4a1d52566c3f60fa
Syohei YOSHIDA <syohex@gmail.com>
2014-09-18 13:45:18 +0900

    Fix typo

--- emacs/init_loader/25_ocaml.el
+++ emacs/init_loader/25_ocaml.el
@@ -1,3 +1,3 @@
-(with-eval-hafter-load 'tuareg
+(with-eval-after-load 'tuareg
   (define-key tuareg-mode-map (kbd "C-c C-z") 'tuareg-run-ocaml)
   (define-key tuareg-mode-map (kbd "M-o") 'editutil-edit-next-line-same-column))
