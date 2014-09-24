628714f1f429c96e52b7c133a49628979633fae7
Syohei YOSHIDA <syohex@gmail.com>
2014-09-24 19:47:41 +0900

    Use ac-cider instead of ac-nrepl. ac-nrepl is obsoleted packagegit st .

--- emacs/init_loader/24_clojure.el
+++ emacs/init_loader/24_clojure.el
@@ -1,7 +1,7 @@
 ;; Clojure
 (with-eval-after-load 'cider
   (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-  (add-hook 'cider-mode-hook 'ac-nrepl-setup)
-  (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+  (add-hook 'cider-mode-hook 'ac-cider-setup)
+  (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
 
   (define-key cider-mode-map (kbd "C-M-i") 'auto-complete))
