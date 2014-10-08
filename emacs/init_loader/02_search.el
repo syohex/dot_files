9c37fefb50ad5f1238d706d1c140f5fd5b7e4ca3
Syohei YOSHIDA <syohex@gmail.com>
2014-10-08 15:42:56 +0900

    Move own command to editutil

--- emacs/init_loader/02_search.el
+++ emacs/init_loader/02_search.el
@@ -1,9 +1,4 @@
 ;;;; setting for searching
-(defun isearch-yank-symbol ()
-  (interactive)
-  (isearch-yank-internal (lambda () (forward-symbol 1) (point))))
-(define-key isearch-mode-map (kbd "C-M-w") 'isearch-yank-symbol)
-
 (with-eval-after-load 'ace-jump-mode
   (setq ace-jump-mode-case-fold nil)
   (set-face-attribute 'ace-jump-face-foreground nil
