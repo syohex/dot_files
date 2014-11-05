d1bd8af9de639f4e220799f5b0c06b3d6e9cdf55
Syohei YOSHIDA <syohex@gmail.com>
2014-11-05 15:59:47 +0900

    Add CMake cache file

--- emacs/init_loader/16_recent.el
+++ emacs/init_loader/16_recent.el
@@ -4,6 +4,7 @@
  '(recentf-auto-cleanup 600)
  '(recentf-exclude '(".recentf" "/elpa/" "/elisps/" "^/tmp/" "/\\.git/" "/\\.cask/"
                      "\\.mime-example" "\\.ido.last" "woman_cache.el"
+                     "CMakeCache.txt"
                      "COMMIT_EDITMSG" "MERGE_MSG" "bookmarks" "\\.gz$")))
 
 (run-at-time t 600 'recentf-save-list)
