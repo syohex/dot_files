97efb8ee913540f07f355e94034cc559355104e6
Syohei YOSHIDA <syohex@gmail.com>
2014-11-15 22:00:28 +0900

    Improve regexp and don't save 'gomi' directory

--- emacs/init_loader/16_recent.el
+++ emacs/init_loader/16_recent.el
@@ -2,7 +2,8 @@
 (custom-set-variables
  '(recentf-max-saved-items 2000)
  '(recentf-auto-cleanup 600)
- '(recentf-exclude '(".recentf" "/elpa/" "/elisps/" "^/tmp/" "/\\.git/" "/\\.cask/"
+ '(recentf-exclude '(".recentf" "/elpa/" "/elisps/" "\\`/tmp/" "/\\.git/" "/\\.cask/"
+                     "/tmp/gomi/"
                      "\\.mime-example" "\\.ido.last" "woman_cache.el"
                      "CMakeCache.txt" "bookmarks" "\\.gz$"
                      "COMMIT_EDITMSG" "MERGE_MSG" "git-rebase-todo")))
