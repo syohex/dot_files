8c5c38ec3db4214fe68c045904f7c40dadc804ca
Syohei YOSHIDA <syohex@gmail.com>
2014-12-11 22:01:30 +0900

    Set newer repository URL

--- golang/setup.sh
+++ golang/setup.sh
@@ -5,7 +5,7 @@ set -x
 
 go get -u github.com/nsf/gocode
 go get -u code.google.com/p/rog-go/exp/cmd/godef
-go get -u code.google.com/p/go.tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/golang/lint/golint
 go get -u github.com/peco/peco/cmd/peco
 go get -u github.com/syohex/gotentry
