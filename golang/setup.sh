61960b21ea806813f3ff75a6941cd65ea045f11d
Syohei YOSHIDA <syohex@gmail.com>
2014-12-24 13:31:17 +0900

    Clean up my tool list

--- golang/setup.sh
+++ golang/setup.sh
@@ -8,11 +8,12 @@ go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/golang/lint/golint
 go get -u github.com/peco/peco/cmd/peco
-go get -u github.com/syohex/gotentry
-go get -u github.com/motemen/ghq
-go get -u github.com/peco/migemogrep
 go get -u github.com/jstemmer/gotags
 go get -u github.com/mitchellh/gox
+
+# My own tools
+go get -u github.com/syohex/gotentry
+go get -u github.com/syohex/url_title
 
 OS=$(uname)
 if [ "x$OS" = "xLinux" ]; then
