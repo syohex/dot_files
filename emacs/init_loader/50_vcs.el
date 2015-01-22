8941424230efa18a2aff11ba3483a884e69ddc9d
Syohei YOSHIDA <syohex@gmail.com>
2015-01-22 13:36:23 +0900

    Change binding C-x v l
    vc-log is too heavy for me.

--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -44,6 +44,8 @@
   (delete-other-windows))
 
 (global-set-key (kbd "M-g M-g") 'magit-status)
+(global-set-key (kbd "C-x v l") 'magit-log)
+
 (with-eval-after-load 'magit
   (advice-add 'magit-status :around 'my/magit-status-around)
 
