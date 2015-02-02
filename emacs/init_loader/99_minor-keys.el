88b3b57a367eba769b8b9cc6d5933a2aaafdd7c1
Syohei YOSHIDA <syohex@gmail.com>
2015-02-02 22:33:31 +0900

    Remove needless binding

--- emacs/init_loader/99_minor-keys.el
+++ emacs/init_loader/99_minor-keys.el
@@ -10,5 +10,4 @@
     (,(kbd "M-e") . editutil-forward-char)
     (,(kbd "M-a") . editutil-backward-char)
     (,(kbd "M-q") . editutil-zap-to-char)
-    (,(kbd "M-Q") . editutil-zap-to-char-backward)
     (,(kbd "C-M-o") . editutil-other-window)))
