9f655bd89ba886ab37589b04aa5b6e8ad5080e52
Syohei YOSHIDA <syohex@gmail.com>
2015-02-20 16:45:30 +0900

    Add 'hub' command

--- golang/setup.sh
+++ golang/setup.sh
@@ -10,6 +10,7 @@ go get -u github.com/golang/lint/golint
 go get -u github.com/peco/peco/cmd/peco
 go get -u github.com/jstemmer/gotags
 go get -u github.com/mitchellh/gox
+go get -u github.com/github/hub
 
 # My own tools
 go get -u github.com/syohex/gotentry
