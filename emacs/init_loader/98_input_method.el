c8c242de5a987c02951e9c372599f73f42062180
Syohei YOSHIDA <syohex@gmail.com>
2015-03-09 10:39:23 +0900

    Check whether mozc helper is installed

--- emacs/init_loader/98_input_method.el
+++ emacs/init_loader/98_input_method.el
@@ -17,6 +17,6 @@
  '(mozc-candidate-style 'echo-area) ;; overlay is too slow
  '(mozc-leim-title "[も]"))
 
-(when (require 'mozc nil t)
+(when (and (require 'mozc nil t) (executable-find "mozc_emacs_helper"))
   (setq default-input-method "japanese-mozc")
   (global-set-key (kbd "C-o") 'toggle-input-method))
