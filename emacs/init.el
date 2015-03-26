e20c5048d1e4d93f6247afb5f5d528da43dbeb98
Syohei YOSHIDA <syohex@gmail.com>
2015-03-26 19:22:43 +0900

    Switch from Cask to el-get

--- emacs/init.el
+++ emacs/init.el
@@ -5,12 +5,137 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
-;; Add load path of emacs lisps
-(add-to-list 'load-path (concat user-emacs-directory "elisps"))
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
-;; Emacs package system
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
+;; setup
+(el-get-bundle emacs-jp/init-loader)
+(el-get-bundle purcell/exec-path-from-shell)
+
+;; Utility
+(el-get-bundle syohex/emacs-editutil :name editutil)
+
+;; Theme
+(el-get-bundle syohex/emacs-syohex-theme :name syohex-theme)
+
+;; Input method
+(el-get-bundle elpa:mozc)
+
+;; undo
+(el-get-bundle undo-tree :type git :url "http://www.dr-qubit.org/git/undo-tree.git")
+
+;; highlighting
+(el-get-bundle vline)
+(el-get-bundle col-highlight)
+
+;; Search
+(el-get-bundle syohex/emacs-anzu :name anzu)
+
+;; moving cursor
+(el-get-bundle goto-chg)
+(el-get-bundle winterTTr/ace-jump-mode)
+
+;; Pair
+(el-get-bundle capitaomorte/autopair)
+(el-get-bundle paredit :type git :url "http://mumble.net/~campbell/git/paredit.git")
+
+;; Region
+(el-get-bundle rejeep/wrap-region)
+
+;; Buffer
+(el-get-bundle emacs-jp/elscreen)
+(el-get-bundle m2ym/popwin-el :name popwin)
+(el-get-bundle lukhas/buffer-move)
+(el-get-bundle syohex/emacs-import-popwin)
+
+;; Directory
+(el-get-bundle m2ym/direx-el :name direx)
+(el-get-bundle syohex/emacs-dired-k :name dired-k)
+(el-get-bundle dired-hacks)
+
+;; auto-complete
+(el-get-bundle auto-complete/popup-el :name popup)
+(el-get-bundle auto-complete/fuzzy-el :name fuzzy)
+(el-get-bundle auto-complete/auto-complete)
+
+;; Repeat utility
+(el-get-bundle myuhe/smartrep.el)
+
+;; snippet
+(el-get-bundle yasnippet)
+
+;; Ocaml
+;;(el-get-bundle tuareg-mode)
+
+;; Haskell
+;;(el-get-bundle haskell/haskell-mode)
+;;(el-get-bundle ghc-mod)
+
+;; CoffeeScript
+(el-get-bundle defunkt/coffee-mode)
+
+;; Perl
+(el-get-bundle jrockway/cperl-mode)
+
+;; Ruby
+(el-get-bundle ruby-block)
+(el-get-bundle ruby-end)
+(el-get-bundle inf-ruby)
+
+;; Emacs Lisp
+(el-get-bundle purcell/elisp-slime-nav)
+
+;; Clojure
+(el-get-bundle clojure-mode)
+(el-get-bundle cider)
+
+;; Build tool
+(el-get-bundle cmake-mode)
+
+;; Validation
+(el-get-bundle flycheck)
+
+;; Markup language
+(el-get-bundle markdown-mode :type git :url "git://jblevins.org/git/markdown-mode.git")
+(el-get-bundle yoshiki/yaml-mode)
+
+;; HTML
+(el-get-bundle fxbois/web-mode)
+(el-get-bundle smihica/emmet)
+
+;; shell
+(el-get-bundle syohex/emacs-quickrun :name quickrun)
+(el-get-bundle syohex/emacs-eshellutil :name eshellutil)
+
+;; Documentation
+(el-get-bundle zeal-at-point)
+
+;; auto-complete plugins
+;;(el-get-bundle qoocku/ac-sly)
+(el-get-bundle clojure-emacs/ac-cider)
+(el-get-bundle nsf/gocode :load-path ("emacs"))
+(el-get-bundle syohex/emacs-ac-ispell :name ac-ispell)
+(el-get-bundle syohex/emacs-ac-etags :name ac-etags)
+(el-get-bundle dgutov/robe)
+(el-get-bundle zk-phi/ac-c-headers)
+
+;; Helm plugins
+(el-get-bundle ShingoFukuyama/helm-swoop)
+(el-get-bundle emacs-helm/helm-descbinds)
+(el-get-bundle syohex/emacs-helm-gtags :name helm-gtags)
+(el-get-bundle syohex/emacs-helm-ag :name helm-ag)
+(el-get-bundle syohex/emacs-helm-pydoc :name helm-pydoc)
+(el-get-bundle syohex/emacs-helm-perldoc :name helm-perldoc)
+(el-get-bundle syohex/emacs-helm-open-github :name helm-open-github)
+(el-get-bundle syohex/emacs-helm-godoc :name helm-godoc)
+
+;; Evil
+(el-get-bundle evil)
 
 ;; load environment variables
 (let ((envs '("PATH" "VIRTUAL_ENV" "GOROOT" "GOPATH")))
