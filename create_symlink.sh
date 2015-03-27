87823307308331fbb0a6d384d7e501f97527dff4
Syohei YOSHIDA <syohex@gmail.com>
2015-03-27 11:58:59 +0900

    Update setup script

--- create_symlink.sh
+++ create_symlink.sh
@@ -15,7 +15,7 @@ mkdir_if_not_exist ~/.emacs.d
 ln -sf ${PWD}/emacs/init.el ~/.emacs.d/init.el
 ln -sf ${PWD}/emacs/my_snippets ~/.emacs.d/
 ln -sf ${PWD}/emacs/ac-dict ~/.emacs.d/
-ln -sf ${PWD}/emacs/Cask ~/.emacs.d
+ln -sf ${PWD}/emacs/init-el-get.el ~/.emacs.d
 
 ## Use Emacs.app instead of pre-installed emacs on MacOSX
 EMACS_CLIENT_APP_PATH='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
