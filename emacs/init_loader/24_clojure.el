548349027ac0947db78f612f8bf570e7cb2fc054
Syohei YOSHIDA <syohex@gmail.com>
2015-04-06 06:53:44 +0900

    Unbind clojure-quit for using my own indent command

--- emacs/init_loader/24_clojure.el
+++ emacs/init_loader/24_clojure.el
@@ -4,4 +4,5 @@
   (add-hook 'cider-mode-hook 'ac-cider-setup)
   (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
 
-  (define-key cider-mode-map (kbd "C-M-i") 'auto-complete))
+  (define-key cider-mode-map (kbd "C-M-i") 'auto-complete)
+  (define-key cider-mode-map (kbd "C-c C-q") nil))
