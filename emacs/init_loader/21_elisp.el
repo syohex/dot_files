dbcb097af4b3d6cdeda6940244306266fe6fc77f
Syohei YOSHIDA <syohex@gmail.com>
2015-04-07 23:57:19 +0900

    Move theme setting to syohex-theme.el

--- emacs/init_loader/21_elisp.el
+++ emacs/init_loader/21_elisp.el
@@ -11,14 +11,5 @@
 (custom-set-variables
  '(eldoc-idle-delay 0.2))
 
-(with-eval-after-load 'eldoc
-  (set-face-attribute 'eldoc-highlight-function-argument nil
-                      :underline t :foreground "green"
-                      :weight 'bold))
-
-;; for regexp color
-(set-face-foreground 'font-lock-regexp-grouping-backslash "#ff1493")
-(set-face-foreground 'font-lock-regexp-grouping-construct "#ff8c00")
-
 ;; Cask
 (add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
--- emacs/init_loader/50_vcs.el
+++ emacs/init_loader/50_vcs.el
@@ -27,9 +27,6 @@
 
 (add-hook 'focus-in-hook 'git-gutter:update-all-windows)
 
-(set-face-background 'git-gutter:deleted  "red")
-(set-face-background 'git-gutter:modified "magenta")
-
 (smartrep-define-key
     global-map  "C-x" '(("p" . 'git-gutter:previous-diff)
                         ("n" . 'git-gutter:next-diff)))
