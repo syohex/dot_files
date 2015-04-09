5acaf58f82706b93417f829ae6a9ad7ef5fc9e9a
Syohei YOSHIDA <syohex@gmail.com>
2015-04-09 11:49:25 +0900

    Change helm-yank binding

--- emacs/init_loader/99_global-keys.el
+++ emacs/init_loader/99_global-keys.el
@@ -17,7 +17,7 @@
 (global-set-key (kbd "C-x C-x") 'helm-find-files)
 (global-set-key (kbd "C-x C-l") 'helm-locate)
 (global-set-key (kbd "C-x C-c") 'helm-M-x)
-(global-set-key (kbd "ESC M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-h a")   'helm-apropos)
 (global-set-key (kbd "C-h m")   'helm-man-woman)
 (global-set-key (kbd "C-x C-i") 'helm-imenu)
