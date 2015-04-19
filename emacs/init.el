b551f7713ba52b8fc9aa02ee7817061c046e5dd5
Syohei YOSHIDA <syohex@gmail.com>
2015-04-19 21:59:22 +0900

    setq-default is better than setq in this case

--- emacs/init.el
+++ emacs/init.el
@@ -3,7 +3,7 @@
   (cd (getenv "HOME")))
 
 (when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+  (setq-default user-emacs-directory (file-name-directory load-file-name)))
 
 (load (concat user-emacs-directory "init-el-get.el"))
 
