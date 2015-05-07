c1a9f35e3c05cc0d81296d2d76f94eacf6f68635
Syohei YOSHIDA <syohex@gmail.com>
2015-05-07 18:30:43 +0900

    use next-branch instead of stable branch

--- emacs/init-el-get.el
+++ emacs/init-el-get.el
@@ -123,7 +123,7 @@
 (el-get-bundle syohex/emacs-eshellutil :name eshellutil)
 
 ;; VCS
-(el-get-bundle magit)
+(el-get-bundle magit/magit :branch "next")
 (el-get-bundle syohex/emacs-git-gutter :name git-gutter)
 
 ;; Documentation
