f53acb308cb1f8d73441fe44c6b056af31b44a7d
Syohei YOSHIDA <syohex@gmail.com>
2015-06-15 20:59:13 +0900

    Fix for new Ubuntu
    And fix some typos

--- package/ubuntu.sh
+++ package/ubuntu.sh
@@ -5,17 +5,13 @@ set -e
 PACKAGES="zsh tmux
     build-essential diffutils
     git mercurial git-svn
-    emacs vim aspell ispell exuberant-ctags ttf-vlgothic nkf lv
+    emacs vim aspell ispell exuberant-ctags fonts-vlgothic nkf lv
     emacs-mozc emacs-mozc-bin mozc-server mozc-utils-gui
-    dia shutter byzanz handrake
+    dia shutter byzanz handbrake
     curl w3m lftp
     minicom
     wmctrl xsel
-    paco sary"
-
-if [ "x_$XDG_CURRENT_DESKTOP" = "x_XFCE" ]; then
-    PACKAGES="$PACKAGES xfce-theme-manager"
-fi
+    paco"
 
 echo "Install packages:"
 sudo apt install $PACKAGES
