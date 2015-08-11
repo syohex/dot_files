5570efe85a6ab2443c4fff42e6011a49d4242fa8
Syohei YOSHIDA <syohex@gmail.com>
2015-08-11 23:43:52 +0900

    Remove ccl configuration

--- create_symlink.sh
+++ create_symlink.sh
@@ -63,7 +63,6 @@ ln -sf $PWD/ruby/pryrc ~/.pryrc
 
 # For Common Lisp
 #ln -sf $PWD/common_lisp/clisprc.lisp ~/.clisprc.lisp
-#ln -sf $PWD/common_lisp/ccl-init.lisp ~/.ccl-init.lisp
 
 # haskell
 ln -sf $PWD/haskell/ghci ~/.ghci
