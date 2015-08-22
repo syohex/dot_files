5e2daad8d65dbbb0df45a78721000761099dce7e
Syohei YOSHIDA <syohex@gmail.com>
2015-08-22 15:40:44 +0900

    Use markdown command instead of snippet for inserting code block

--- emacs/init/markdown.el
+++ emacs/init/markdown.el
@@ -17,4 +17,6 @@
   (define-key markdown-mode-map (kbd "C-c C-p") 'outline-previous-visible-heading)
   (define-key markdown-mode-map (kbd "C-c C-f") 'outline-forward-same-level)
   (define-key markdown-mode-map (kbd "C-c C-b") 'outline-backward-same-level)
-  (define-key markdown-mode-map (kbd "C-c C-u") 'outline-up-heading))
+  (define-key markdown-mode-map (kbd "C-c C-u") 'outline-up-heading)
+
+  (define-key gfm-mode-map (kbd "C-c C-i c") 'markdown-insert-gfm-code-block))
