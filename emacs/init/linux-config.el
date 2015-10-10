8d9c8e67bb21a634e032b9bca251126def3ec64c
Syohei YOSHIDA <syohex@gmail.com>
2015-10-10 23:35:54 +0900

    Use built-in command instead of external x window command

--- emacs/init/linux-config.el
+++ emacs/init/linux-config.el
@@ -12,14 +12,9 @@
    (list (read-number "Size: ")))
   (set-frame-font (format "VL ゴシック-%d" size)))
 
-(with-temp-buffer
-  (call-process "xdpyinfo" nil t)
-  (goto-char (point-min))
-  (when (re-search-forward "dimensions:\\s-+\\([0-9]+\\)x\\(?:[0-9]+\\)" nil t)
-    (let* ((width (string-to-number (match-string-no-properties 1)))
-           (size (if (>= width 1900) 12 10)))
-      (condition-case err
-          (let ((myfont (format "VL ゴシック-%d" size)))
-            (set-frame-font myfont)
-            (add-to-list 'default-frame-alist `(font . ,myfont)))
-        (error (message "%s" err))))))
+(let ((size (if (>= (x-display-pixel-width) 1900) 14 10)))
+  (condition-case err
+      (let ((myfont (format "VL ゴシック-%d" size)))
+	(set-frame-font myfont)
+	(add-to-list 'default-frame-alist `(font . ,myfont)))
+    (error (message "%s" err))))
