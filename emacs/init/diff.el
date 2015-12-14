08a0b46ed7ba5516698cfce6048a2eb63568b791
Syohei YOSHIDA <syohex@gmail.com>
2015-12-14 23:27:13 +0900

    Remove diff-mode bindings

--- emacs/init/diff.el
+++ emacs/init/diff.el
@@ -1,9 +1,3 @@
-;; setting for diff-mode and ediff
-(with-eval-after-load 'diff-mode
-  ;; key bindings
-  (define-key diff-mode-map (kbd "C-M-n") 'diff-file-next)
-  (define-key diff-mode-map (kbd "C-M-p") 'diff-file-prev))
-
 ;; ediff
 (custom-set-variables
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
