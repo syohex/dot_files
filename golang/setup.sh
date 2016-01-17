f4372770a66abf321efa65232f5ac59e527cef37
Syohei YOSHIDA <syohex@gmail.com>
2016-01-17 23:30:37 +0900

    Remove duplicated lines and sort lines

--- golang/setup.sh
+++ golang/setup.sh
@@ -3,20 +3,19 @@
 set -e
 set -x
 
-go get -u github.com/nsf/gocode
-go get -u golang.org/x/tools/cmd/godoc
-go get -u github.com/rogpeppe/godef
-go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/github/hub
 go get -u github.com/golang/lint/golint
-go get -u github.com/peco/peco/cmd/peco
+go get -u github.com/josharian/impl
 go get -u github.com/jstemmer/gotags
 go get -u github.com/mitchellh/gox
-go get -u github.com/github/hub
-go get -u github.com/motemen/gore
 go get -u github.com/motemen/ghq
-go get -u github.com/josharian/impl
+go get -u github.com/motemen/gore
+go get -u github.com/nsf/gocode
+go get -u github.com/peco/peco/cmd/peco
+go get -u github.com/rogpeppe/godef
 go get -u golang.org/x/tools/cmd/cover
 go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/vet
 
 OS=$(uname)
