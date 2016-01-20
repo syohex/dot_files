6c4024a4eba3c243e79a5cb3f160bbece4134206
Syohei YOSHIDA <syohex@gmail.com>
2016-01-20 13:31:31 +0900

    Update exclude list
    - .recentf is now recentf
    - Fix unescaped meta character issue

--- emacs/init/recent.el
+++ emacs/init/recent.el
@@ -2,8 +2,8 @@
 (custom-set-variables
  '(recentf-max-saved-items 2000)
  '(recentf-auto-cleanup 600)
- '(recentf-exclude '(".recentf" "/elpa/" "/elisps/" "\\`/tmp/" "/\\.git/" "/\\.cask/"
-                     "/tmp/gomi/" "/el-get/" ".loaddefs.el" "/.cpanm/"
+ '(recentf-exclude '("recentf" "/elpa/" "/elisps/" "\\`/tmp/" "/\\.git/" "/\\.cask/"
+                     "/tmp/gomi/" "/el-get/" ".loaddefs.el" "/\\.cpanm/"
                      "\\.mime-example" "\\.ido.last" "woman_cache.el"
                      "CMakeCache.txt" "/bookmarks" "\\.gz$"
                      "COMMIT_EDITMSG" "MERGE_MSG" "git-rebase-todo")))
