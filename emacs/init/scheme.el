3cafc28786353bad8f8405f44e75284c3c673305
Syohei YOSHIDA <syohex@gmail.com>
2016-02-12 00:21:05 +0900

    Update scheme configuration
    - Update newer Emacs syntax
    - Better default value

--- emacs/init/scheme.el
+++ emacs/init/scheme.el
@@ -2,18 +2,17 @@
 (custom-set-variables
  '(scheme-program-name "gosh"))
 
-(eval-after-load "scheme"
-  '(progn
-     (require 'cmuscheme)
-     (push '("*scheme*" :stick t) popwin:special-display-config)
-     (define-key scheme-mode-map (kbd "C-c C-z") 'run-scheme)
-     (define-key scheme-mode-map (kbd "C-c C-d") 'my/gauche-info-index)))
+(with-eval-after-load "scheme"
+  (require 'cmuscheme)
+  (push '("*scheme*" :stick t) popwin:special-display-config)
+  (define-key scheme-mode-map (kbd "C-c C-z") 'run-scheme)
+  (define-key scheme-mode-map (kbd "C-c C-d") 'my/gauche-info-index))
 
 (defun my/gauche-info-index (topic)
   (interactive
    (list (read-string
 	  (concat "Gauche help topic : ")
-          (current-word))))
+          (or (current-word) ""))))
   (switch-to-buffer-other-window (get-buffer-create "*info*"))
   (let ((info-file "/usr/share/info/gauche-refe.info.gz"))
     (info info-file)
