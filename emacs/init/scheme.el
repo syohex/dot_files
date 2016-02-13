e8b247a5c57dada7d83d812efde635af4cf6df94
Syohei YOSHIDA <syohex@gmail.com>
2016-02-13 23:32:15 +0900

    Use helm-info command instead of own command

--- emacs/init/scheme.el
+++ emacs/init/scheme.el
@@ -4,16 +4,7 @@
 
 (with-eval-after-load "scheme"
   (require 'cmuscheme)
+  (require 'helm-info)
   (push '("*scheme*" :stick t) popwin:special-display-config)
   (define-key scheme-mode-map (kbd "C-c C-z") 'run-scheme)
-  (define-key scheme-mode-map (kbd "C-c C-d") 'my/gauche-info-index))
-
-(defun my/gauche-info-index (topic)
-  (interactive
-   (list (read-string
-	  (concat "Gauche help topic : ")
-          (or (current-word) ""))))
-  (switch-to-buffer-other-window (get-buffer-create "*info*"))
-  (let ((info-file "/usr/share/info/gauche-refe.info.gz"))
-    (info info-file)
-    (Info-index topic)))
+  (define-key scheme-mode-map (kbd "C-c C-d") 'helm-info-gauche-refe))
