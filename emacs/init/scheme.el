6e6227d020ca5ab5c392d7d52a406b84701427fd
Syohei YOSHIDA <syohex@gmail.com>
2016-02-14 14:03:48 +0900

    Update scheme-mode binding

--- emacs/init/scheme.el
+++ emacs/init/scheme.el
@@ -7,4 +7,6 @@
   (require 'helm-info)
   (push '("*scheme*" :stick t) popwin:special-display-config)
   (define-key scheme-mode-map (kbd "C-c C-z") 'run-scheme)
+  (define-key scheme-mode-map (kbd "C-c C-c") 'scheme-send-definition)
+  (define-key scheme-mode-map (kbd "C-c C-e") 'scheme-send-definition-and-go)
   (define-key scheme-mode-map (kbd "C-c C-d") 'helm-info-gauche-refe))
