4921e2a22371cad898d7c9aa43feeea0b8564811
Syohei YOSHIDA <syohex@gmail.com>
2016-02-21 11:30:08 +0900

    Use own document command instead of helm-info

--- emacs/init/scheme.el
+++ emacs/init/scheme.el
@@ -1,12 +1,10 @@
 ;; setting for scheme
 (with-eval-after-load "scheme"
   (require 'cmuscheme)
-  (require 'helm-info)
 
   (progutil-scheme-setup 'gauche)
 
   (push '("*scheme*" :stick t) popwin:special-display-config)
   (define-key scheme-mode-map (kbd "C-c C-z") 'run-scheme)
   (define-key scheme-mode-map (kbd "C-c C-c") 'scheme-send-definition)
-  (define-key scheme-mode-map (kbd "C-c C-e") 'scheme-send-definition-and-go)
-  (define-key scheme-mode-map (kbd "C-c C-d") 'helm-info-gauche-refe))
+  (define-key scheme-mode-map (kbd "C-c C-e") 'scheme-send-definition-and-go))
