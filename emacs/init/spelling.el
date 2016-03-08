f35b72ef4933203e91258fcf8ea7ef23a21d415d
Syohei YOSHIDA <syohex@gmail.com>
2016-03-08 12:29:16 +0900

    Don't binding C-M-i for flyspell command

--- emacs/init/spelling.el
+++ emacs/init/spelling.el
@@ -1,3 +1,6 @@
 ;; configuration of spell check
+(custom-set-variables
+ '(flyspell-use-meta-tab nil))
+
 (with-eval-after-load 'ispell
   (add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
