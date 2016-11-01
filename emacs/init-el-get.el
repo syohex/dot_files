6e927456d1bc8eef7ff0a6ae9451e73805f76f93
Syohei YOSHIDA <syohex@gmail.com>
2016-11-02 00:59:24 +0900

    Remove workaround Emacs 25

--- emacs/init-el-get.el
+++ emacs/init-el-get.el
@@ -111,9 +111,6 @@
 ;;(el-get-bundle tonini/alchemist.el)
 
 ;; Clojure
-(el-get-bundle queue
-  :type http
-  :url "https://elpa.gnu.org/packages/queue-0.1.1.el")
 (el-get-bundle clojure-mode)
 (el-get-bundle cider)
 (el-get-bundle clj-refactor)
