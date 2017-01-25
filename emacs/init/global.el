6b53aa26b4d955f2fccee802526547acc7a38c2b
Syohei YOSHIDA <syohex@gmail.com>
2017-01-25 17:44:56 +0900

    Add helm-gtags-select binding

--- emacs/init/global.el
+++ emacs/init/global.el
@@ -6,6 +6,7 @@
   (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
   (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
   (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+  (define-key helm-gtags-mode-map (kbd "M-g M-s") 'helm-gtags-select)
   (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
   (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
   (define-key helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack))
