269149155be6cff99375c0e70d7bae2c23d2219d
Shohei YOSHIDA <syohex@gmail.com>
2020-03-16 11:49:18 +0900

    Better setup script

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 set -e
 set -x
 
@@ -14,7 +13,11 @@ cd $UTILDIR
 (git clone git@github.com:syohex/my-command-utilities.git && cd my-command-utilities && ./setup.sh)
 
 # setting for zsh zaw
-(cd "${HOME}/.zsh" && git clone git://github.com/zsh-users/zaw.git)
+if [[ -d ${HOME}/.zsh/zaw ]]; then
+    (cd "${HOME}/.zsh" && git pull --rebase origin master)
+else
+    (cd "${HOME}/.zsh" && git clone git://github.com/zsh-users/zaw.git)
+fi
 ## Install 3rd party zaw source
 (cd "${HOME}/.zsh/zaw/sources" && curl -O https://raw.github.com/syohex/zaw-git-directories/master/git-directories.zsh )
 (cd "${HOME}/.zsh/zaw/sources" && curl -O https://raw.github.com/shibayu36/config-file/master/.zsh/zaw-sources/git-recent-branches.zsh)
