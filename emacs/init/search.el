a16fbdfe524b5b5b7700e0acf174de20d464241c
Shohei YOSHIDA <syohex@gmail.com>
2020-05-15 07:39:29 +0900

    Use default value instead

--- emacs/init/search.el
+++ emacs/init/search.el
@@ -4,6 +4,4 @@
 (custom-set-variables
  '(anzu2-mode-lighter "")
  '(anzu2-deactivate-region t)
- '(anzu2-search-threshold 1000)
- '(anzu2-replace-threshold 50)
  '(anzu2-replace-to-string-separator " => "))
