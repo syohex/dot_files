8ca78c48b5fed7d2ab173e4dcfa51689784a05b9
Shohei YOSHIDA <syohex@gmail.com>
2020-05-29 09:36:42 +0900

    Remove needless key binding

--- emacs/init/markdown.el
+++ emacs/init/markdown.el
@@ -8,13 +8,7 @@
 
 (with-eval-after-load 'markdown-mode
   ;; key bindings
-  (define-key markdown-mode-map (kbd "C-M-i") 'company-complete)
-
-  (define-key markdown-mode-map (kbd "C-c C-c C-l") 'markdown-insert-link)
-  (define-key markdown-mode-map (kbd "C-c C-c C-i") 'markdown-insert-image)
-  (define-key markdown-mode-map (kbd "C-c .") 'org-time-stamp)
-
-  (define-key gfm-mode-map (kbd "C-c C-c C-c") 'markdown-insert-gfm-code-block))
+  (define-key markdown-mode-map (kbd "C-M-i") 'company-complete))
 
 (defun my/markdown-mode-hook ()
   (setq-local company-backends '(company-ispell company-files company-dabbrev))
