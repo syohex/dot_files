cc9c9d014c7906e6d1f08e993f5acb35a4918b37
Shohei YOSHIDA <syohex@gmail.com>
2020-07-22 01:56:39 +0900

    Add new binding

--- emacs/init/global.el
+++ emacs/init/global.el
@@ -7,6 +7,7 @@
   (define-key helm-gtags2-mode-map (kbd "M-g M-s") #'helm-gtags2-select)
   (define-key helm-gtags2-mode-map (kbd "C-c >") #'helm-gtags2-next-history)
   (define-key helm-gtags2-mode-map (kbd "C-c <") #'helm-gtags2-previous-history)
+  (define-key helm-gtags2-mode-map (kbd "C-c z") #'helm-gtags2-tag-continue)
   (define-key helm-gtags2-mode-map (kbd "C-t") #'helm-gtags2-pop-stack))
 
 ;;; Enable helm-gtags-mode
