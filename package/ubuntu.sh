f64b66797de96f472e8793ba5bbe810a3def8ade
Shohei YOSHIDA <syohex@gmail.com>
2020-12-15 01:10:54 +0900

    Update packages

--- package/ubuntu.sh
+++ package/ubuntu.sh
@@ -4,14 +4,13 @@ set -e
 
 PACKAGES="zsh tmux
     build-essential diffutils
-    git mercurial git-svn
+    git
     emacs vim aspell ispell exuberant-ctags fonts-vlgothic nkf lv
     emacs-mozc emacs-mozc-bin mozc-server mozc-utils-gui
-    dia shutter byzanz handbrake
+    dia byzanz
     curl w3m lftp jq
-    minicom
     wmctrl xsel
-    paco"
+    porg"
 
 echo "Install packages:"
 sudo apt install $PACKAGES
