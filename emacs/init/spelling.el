2a97b34a964adeee8e841bd8df7c10cdc7632234
Shohei YOSHIDA <syohex@gmail.com>
2021-02-21 12:48:52 +0900

    Enable camel case option for spell check

--- emacs/init/spelling.el
+++ emacs/init/spelling.el
@@ -4,4 +4,7 @@
  '(flyspell-use-meta-tab nil))
 
 (with-eval-after-load 'ispell
-  (add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+  (add-to-list 'ispell-skip-region-alist '("[^\000-\377]+"))
+  (when (and ispell-program-name
+             (and (string= (file-name-nondirectory ispell-program-name) "aspell")))
+    (setq ispell-extra-args '("--lang=en_US" "--camel-case"))))
