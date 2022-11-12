36e624e30ae3d2a3849492a4f498c7a543975047
Shohei YOSHIDA <syohex@gmail.com>
2022-11-13 00:48:41 +0900

    Cleanup code

--- minimum/init.el
+++ minimum/init.el
@@ -43,6 +43,14 @@
  '(show-paren-style 'expression)
  '(view-read-only t)
  '(sh-indentation 2)
+ '(auto-revert-interval 10)
+ '(auto-revert-check-vc-info t)
+ '(vc-follow-symlinks t)
+ '(vc-handled-backends '(Git))
+ '(recentf-max-saved-items 1000)
+ '(recentf-exclude '(".recentf" "/repos/" "/elpa/" "/el-get/" "CMakeCache.txt"
+                     "/usr/local/share/emacs/"
+                     "\\.mime-example" "\\.ido.last" "/tmp/gomi/" "/\\.cpanm/"))
  '(warning-suppress-types '((el-get) ((package reinitialization)))))
 
 (setq gc-cons-threshold (* gc-cons-threshold 10)
@@ -74,7 +82,7 @@
 
 (el-get-bundle emacsmirror/clang-format)
 (el-get-bundle dominikh/go-mode.el)
-(el-get-bundle fsharp/emacs-fsharp-mode)
+(el-get-bundle fsharp/emacs-fsharp-mode :name fsharp-mode)
 (el-get-bundle rust-lang/rust-mode)
 (el-get-bundle kotlin-mode)
 (el-get-bundle haskell/haskell-mode)
@@ -83,28 +91,6 @@
 (prefer-coding-system 'utf-8-unix)
 
 (require 'cl-lib)
-
-(dolist (hook '(emacs-lisp-mode-hook
-                lisp-interaction-mode-hook
-                lisp-mode-hook))
-  (add-hook hook 'enable-paredit-mode))
-
-(dolist (hook '(c-mode-hook
-                c++-mode-hook
-                python-mode-hook
-                fsharp-mode-hook
-                kotlin-mode-hook
-                go-mode-hook
-                sh-mode-hook
-                js-mode-hook
-                rust-mode-hook
-                css-mode-hook
-                cmake-mode-hook
-                cperl-mode-hook
-                markdown-mode-hook
-                gfm-mode-hook
-                yaml-mode-hook))
-  (add-hook hook 'electric-pair-local-mode))
 
 (require 'smartrep)
 
@@ -122,9 +108,13 @@
 (global-set-key (kbd "C-M-z") 'helm-resume)
 (global-set-key (kbd "C-x C-i") 'helm-imenu)
 (global-set-key (kbd "C-x C-c") 'helm-M-x)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+(global-set-key (kbd "C-x v d") 'vc-diff)
 (global-set-key (kbd "M-g .") 'helm-ag2)
 (global-set-key (kbd "M-g ,") 'helm-ag2-pop-stack)
 (global-set-key (kbd "M-g p") 'helm-ag2-project-root)
+(global-set-key (kbd "M-g f") 'helm-do-ag2-project-root)
 (global-set-key (kbd "M-g M-f") 'ffap)
 (global-set-key (kbd "M-g M-l") 'flymake-show-buffer-diagnostics)
 
@@ -152,8 +142,47 @@
 (defalias 'exit 'save-buffers-kill-emacs)
 
 ;; backspace
-(when (not window-system)
-  (normal-erase-is-backspace-mode 0))
+(normal-erase-is-backspace-mode -1)
+
+(require 'uniquify)
+(require 'recentf)
+(run-at-time t 600 'recentf-save-list)
+(recentf-mode +1)
+
+(global-auto-revert-mode 1)
+
+(dolist (hook '(emacs-lisp-mode-hook
+                lisp-interaction-mode-hook
+                lisp-mode-hook))
+  (add-hook hook 'enable-paredit-mode))
+
+(dolist (hook '(c-mode-hook
+                c++-mode-hook
+                perl-mode-hook
+                python-mode-hook
+                fsharp-mode-hook
+                kotlin-mode-hook
+                go-mode-hook
+                sh-mode-hook
+                js-mode-hook
+                rust-mode-hook
+                css-mode-hook
+                cmake-mode-hook
+                cperl-mode-hook
+                markdown-mode-hook
+                gfm-mode-hook
+                yaml-mode-hook))
+  (add-hook hook 'electric-pair-local-mode))
+
+
+(dolist (hook '(c-mode-hook
+                c++-mode-hook
+                go-mode-hook
+                fsharp-mode-hook
+                haskell-mode-hook
+                python-mode-hook
+                rust-mode-hook))
+  (add-hook hook #'eglot-ensure))
 
 (with-eval-after-load 'cc-mode
   (define-key c-mode-map (kbd "M-q") 'nil)
@@ -173,22 +202,26 @@
 (add-hook 'c-mode-hook 'my/c-mode-hook)
 (add-hook 'c++-mode-hook 'my/c-mode-hook)
 
-(add-hook 'asm-mode-hook 'helm-gtags2-mode)
+(with-eval-after-load 'fsharp-mode
+  (require 'eglot-fsharp))
 
-;; ibuffer
-(defalias 'list-buffers 'ibuffer)
+(add-to-list 'auto-mode-alist '("\\.t\\'" . perl-mode))
 
-;;;; dired
-(require 'dired)
-;; Not create new buffer, if you chenge directory in dired
-(put 'dired-find-alternate-file 'disabled nil)
-(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
-(define-key dired-mode-map (kbd "r") 'wdired-change-to-wdired-mode)
-(define-key dired-mode-map (kbd "C-M-u") 'dired-up-directory)
-(define-key dired-mode-map (kbd "K") 'dired-k)
-(global-set-key (kbd "C-x C-j") #'dired-jump)
-;; display directories by first
-(load-library "ls-lisp")
+(with-eval-after-load 'dired
+  ;; Not create new buffer, if you chenge directory in dired
+  (put 'dired-find-alternate-file 'disabled nil)
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+  (define-key dired-mode-map (kbd "r") 'wdired-change-to-wdired-mode)
+  (define-key dired-mode-map (kbd "C-M-u") 'dired-up-directory)
+  ;; display directories by first
+  (load-library "ls-lisp"))
+
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "C-x n") nil)
+  (define-key markdown-mode-map (kbd "C-x n b") nil)
+  (define-key markdown-mode-map (kbd "C-x n s") nil))
+
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
 ;; helm
 (require 'helm-config)
@@ -198,8 +231,14 @@
 (helm-mode -1)
 
 (define-key helm-map (kbd "C-q") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-p") 'helm-previous-line)
+(define-key helm-map (kbd "C-n") 'helm-next-line)
+(define-key helm-map (kbd "C-M-p") 'helm-previous-source)
+(define-key helm-map (kbd "C-M-n") 'helm-next-source)
 
-(require 'helm-descbinds)
+(global-set-key (kbd "C-M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-h a") 'helm-apropos)
+
 (helm-descbinds-install)
 
 ;; gtags
@@ -211,47 +250,10 @@
   (define-key helm-gtags2-mode-map (kbd "C-c <") 'helm-gtags2-previous-history)
   (define-key helm-gtags2-mode-map (kbd "C-t") 'helm-gtags2-pop-stack))
 
-(define-key helm-map (kbd "C-p") 'helm-previous-line)
-(define-key helm-map (kbd "C-n") 'helm-next-line)
-(define-key helm-map (kbd "C-M-p") 'helm-previous-source)
-(define-key helm-map (kbd "C-M-n") 'helm-next-source)
-
-(global-set-key (kbd "C-M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-h a") 'helm-apropos)
-
-;; helm faces
 (require 'helm-files)
 (setq helm-find-files-doc-header "")
 
-;; naming of same name file
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-
-(setq recentf-max-saved-items 1000)
-(require 'recentf)
-(run-at-time t 600 'recentf-save-list)
-(setq recentf-exclude '(".recentf" "/repos/" "/elpa/" "/el-get/" "CMakeCache.txt"
-                        "/usr/local/share/emacs/"
-                        "\\.mime-example" "\\.ido.last" "/tmp/gomi/" "/\\.cpanm/"))
-(recentf-mode +1)
-
-;; for virsion control system
-(global-auto-revert-mode 1)
-(setq auto-revert-interval 10
-      auto-revert-check-vc-info t
-      vc-follow-symlinks t
-      vc-handled-backends '(Git))
-
-;; which-func
-(require 'which-func)
-(set-face-foreground 'which-func "color-201")
-(set-face-bold-p 'which-func t)
-(which-function-mode t)
-
-(global-set-key (kbd "C-x v d") 'vc-diff)
-
-;; company-mode
-(require 'company)
+(which-function-mode +1)
 
 (global-company-mode +1)
 (global-set-key (kbd "C-M-i") 'company-complete)
@@ -265,11 +267,8 @@
 (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
 
 (which-key-mode +1)
-
-;; anzu
 (global-anzu2-mode +1)
 
-;; editutil
 (require 'editutil)
 (editutil-default-setup)
 
@@ -283,32 +282,7 @@
     (,(kbd "M-a") . editutil-backward-char)
     (,(kbd "M-e") . editutil-forward-char)))
 
-;; view-mode
-(with-eval-after-load 'view
-  (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
-  (define-key view-mode-map (kbd "?") 'View-search-regexp-backward?)
-  (define-key view-mode-map (kbd "g") 'View-goto-line)
-  (define-key view-mode-map (kbd "G") 'editutil-goto-last-line)
-  (define-key view-mode-map (kbd "f") 'editutil-forward-char)
-  (define-key view-mode-map (kbd "F") 'editutil-backward-char)
-  (define-key view-mode-map (kbd "w") 'forward-word)
-  (define-key view-mode-map (kbd "b") 'backward-word)
-
-  (define-key view-mode-map (kbd "i") 'editutil-view-insert)
-  (define-key view-mode-map (kbd "a") 'editutil-view-insert-at-next)
-  (define-key view-mode-map (kbd "I") 'editutil-view-insert-at-bol)
-  (define-key view-mode-map (kbd "A") 'editutil-view-insert-at-eol)
-
-  (define-key view-mode-map (kbd "h") 'backward-char)
-  (define-key view-mode-map (kbd "j") 'next-line)
-  (define-key view-mode-map (kbd "k") 'previous-line)
-  (define-key view-mode-map (kbd "l") 'forward-char)
-  (define-key view-mode-map (kbd "[") 'backward-paragraph)
-  (define-key view-mode-map (kbd "]") 'forward-paragraph))
-
-;; git-gutter
 (global-git-gutter2-mode t)
-
 (global-set-key (kbd "C-x v =") 'git-gutter2-popup-hunk)
 (global-set-key (kbd "C-x v u") 'git-gutter2-update)
 (global-set-key (kbd "C-x v r") 'git-gutter2-revert-hunk)
@@ -322,19 +296,6 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(with-eval-after-load 'markdown-mode
-  (define-key markdown-mode-map (kbd "C-x n") nil)
-  (define-key markdown-mode-map (kbd "C-x n b") nil)
-  (define-key markdown-mode-map (kbd "C-x n s") nil))
-
-(dolist (hook '(c-mode-hook
-                c++-mode-hook
-                go-mode-hook
-                haskell-mode-hook
-                python-mode-hook
-                rust-mode-hook))
-  (add-hook hook #'eglot-ensure))
-
 (set-face-attribute 'mode-line nil
                     :foreground "color-248"
                     :background "color-238")
@@ -342,6 +303,9 @@
                     :foreground "color-254"
                     :background "color-247")
 (set-face-attribute 'mode-line-buffer-id nil
+                    :weight 'extra-bold)
+(set-face-attribute 'which-func nil
+                    :foreground "color-201"
                     :weight 'extra-bold)
 (set-face-attribute 'editutil-clean-space nil
                     :foreground "purple")
@@ -358,8 +322,11 @@
 (set-face-foreground 'font-lock-regexp-grouping-backslash "#ff1493")
 (set-face-foreground 'font-lock-regexp-grouping-construct "#ff8c00")
 (set-face-attribute 'helm-grep-file nil
-                    :foreground "cyan"
+                    :foreground "color-45"
                     :underline 'unspecified)
+(set-face-attribute 'helm-selection nil
+                    :foreground "black"
+                    :background "color-204")
 
 (with-eval-after-load 'eglot
   (set-face-attribute 'eglot-mode-line nil
