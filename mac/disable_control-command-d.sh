4e40f58ccdd88d24519d301463b8bebe009d62ff
Shohei YOSHIDA <syohex@gmail.com>
2022-11-13 23:24:56 +0900

    Remove macOS configuration

--- mac/disable_control-command-d.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-## Disable Control-Command-D for Emacs
-defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 70 '<dict><key>enabled</key><false/></dict>'
-
-echo "*** System restart required ***"
