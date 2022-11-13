103611f3a3c9327b9de6e0a0697716125f9e2a52
Shohei YOSHIDA <syohex@gmail.com>
2022-11-13 23:26:24 +0900

    remove macOS setup script

--- package/mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Install homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-
-sudo brew install zsh git ag coreutils
-sudo brew install aspell --lang=en
