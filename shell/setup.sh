7a666e3f2faabf62c18c0852eecd083e92050b58
Shohei YOSHIDA <syohex@gmail.com>
2023-09-05 15:05:29 +0900

    Update zsh setup script

--- shell/setup.sh
+++ shell/setup.sh
@@ -7,24 +7,19 @@ install -d "${HOME}/.zsh/completions"
 
 # my utilities
 dev_dir="${HOME}/dev"
-install -d $dev_dir
+install -d $dev_dir/zsh
 
-if [[ -d ${dev_dir}/my-command-utilities ]]; then
-  (cd ${dev_dir}/my-command-utilities && git pull --rebase origin master)
-else
+if [[ ! -d ${dev_dir}/my-command-utilities ]]; then
   (cd ${dev_dir} && git clone https://github.com/syohex/my-command-utilities.git && cd my-command-utilities && ./setup.sh)
 fi
 
-# completion
-if [[ -d ${HOME}/.zsh/zsh-completions ]]; then
-  (cd "${HOME}/.zsh/zsh-completions" && git pull --rebase origin master)
-else
-  (cd "${HOME}/.zsh" && git clone https://github.com/zsh-users/zsh-completions)
+# zsh-completions
+if [[ ! -d ${dev_dir}/zsh/zsh-completions ]]; then
+  (cd "${dev_dir}/zsh" && git clone https://github.com/zsh-users/zsh-completions)
+  ln -sf ${dev_dir}/zsh/zsh-completions ~/.zsh/
 fi
 
 # setting for zsh zaw
-if [[ -d ${HOME}/.zsh/zaw ]]; then
-  (cd "${HOME}/.zsh/zaw" && git pull --rebase origin master)
-else
+if [[ ! -d ${HOME}/.zsh/zaw ]]; then
   (cd "${HOME}/.zsh" && git clone https://github.com/zsh-users/zaw)
 fi
