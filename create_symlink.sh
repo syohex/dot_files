cf7b14abce63a7ba4adedc6281757709b275bfdf
Shohei YOSHIDA <syohex@gmail.com>
2024-09-02 00:14:42 +0900

    Consider WSL

--- create_symlink.sh
+++ create_symlink.sh
@@ -18,9 +18,11 @@ PECO_DIR=~/.config/peco
 install -d ${PECO_DIR}
 ln -sf $PWD/golang/config.json ${PECO_DIR}/config.json
 
-ln -sf $PWD/idea/ideavimrc ~/.ideavimrc
+if [[ -n $WSLENV ]]; then
+  ln -sf $PWD/idea/ideavimrc ~/.ideavimrc
 
-VSCODE_USER_DIR=~/.config/Code/User
-install -d ${VSCODE_USER_DIR}
-ln -sf $PWD/vscode/settings.json ${VSCODE_USER_DIR}/settings.json
-ln -sf $PWD/vscode/keybindings.json ${VSCODE_USER_DIR}/keybindings.json
+  VSCODE_USER_DIR=~/.config/Code/User
+  install -d ${VSCODE_USER_DIR}
+  ln -sf $PWD/vscode/settings.json ${VSCODE_USER_DIR}/settings.json
+  ln -sf $PWD/vscode/keybindings.json ${VSCODE_USER_DIR}/keybindings.json
+fi
