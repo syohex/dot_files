87b706fb147a5d808ef2925288cb6c859c2cf46d
Shohei YOSHIDA <syohex@gmail.com>
2025-07-29 14:59:17 +0900

    Improve for WSL environment

--- create_symlink.sh
+++ create_symlink.sh
@@ -19,7 +19,7 @@ PECO_DIR=~/.config/peco
 install -d ${PECO_DIR}
 ln -sf $PWD/golang/config.json ${PECO_DIR}/config.json
 
-if [[ -z $WSLENV ]]; then
+if [[ ! -v WSLENV ]]; then
   ln -sf $PWD/idea/ideavimrc ~/.ideavimrc
 
   VSCODE_USER_DIR=~/.config/Code/User
