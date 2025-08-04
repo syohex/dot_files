b87deb7c0f97a26267121d24c617098efe295ad4
Shohei YOSHIDA <syohex@gmail.com>
2025-08-04 10:01:00 +0900

    Update package list

--- package/ubuntu.sh
+++ package/ubuntu.sh
@@ -3,11 +3,7 @@
 set -e
 
 declare -a packages
-packages=(zsh build-essential diffutils vim aspell jq nkf curl porg bat)
-
-if [[ -z $WSLENV && -n $DISPLAY ]]; then
-  packages+=(emacs-moz emacs-mozc-bin mozc-server mozc-utils-gui)
-fi
+packages=(zsh build-essential diffutils vim aspell jq nkf curl bat ripgrep)
 
 echo "Install packages:"
 sudo apt install "${packages[@]}"
