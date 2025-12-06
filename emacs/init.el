08d70f4af4eab3834f730b89743799d58326790f
Shohei YOSHIDA <syohex@gmail.com>
2025-12-06 17:42:36 +0900

    change log-edit-hook

--- emacs/init.el
+++ emacs/init.el
@@ -39,6 +39,7 @@
 (use-package vc
   :config
   (setopt vc-follow-symlinks t
+          log-edit-hook '(log-edit-insert-message-template log-edit-maybe-show-diff)
           vc-handled-backends '(Git)
           vc-git-diff-switches '("--stat")
           vc-git-log-switches '("--stat")))
